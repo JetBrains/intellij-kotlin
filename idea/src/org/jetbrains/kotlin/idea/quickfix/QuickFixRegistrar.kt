4dbcfa5c32dc253fdaf085aee58306ec081bd569
Toshiaki Kameyama <kmymtsak@gmail.com>
2019-10-22 23:31:21 +0900

    Add quick fix for INLINE_CLASS_CONSTRUCTOR_NOT_FINAL_READ_ONLY_PARAMETER
    ^KT-34533 Fixed
    
    Original commit: 75e8849c94cfbd196c2a9d63848886717f153098
    
    GitOrigin-RevId: b326d768b737271da53b84dead8659a212ec82dd

--- idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
+++ idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
@@ -675,5 +675,9 @@ class QuickFixRegistrar : QuickFixContributor {
         INCOMPATIBLE_THROWS_OVERRIDE.registerFactory(RemoveAnnotationFix)
 
         COMPATIBILITY_WARNING.registerFactory(UseFullyQualifiedCallFix)
+
+        INLINE_CLASS_CONSTRUCTOR_NOT_FINAL_READ_ONLY_PARAMETER.registerFactory(InlineClassConstructorNotValParameterFactory)
+
+        SUBCLASS_CANT_CALL_COMPANION_PROTECTED_NON_STATIC.registerFactory(AddJvmStaticAnnotationFix)
     }
 }
