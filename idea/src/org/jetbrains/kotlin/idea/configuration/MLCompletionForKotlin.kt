0030422f5e5b0580ea846e42d154db5571607d17
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2020-10-29 00:15:52 +0900

    Revert "Migrate from `completion-stats` to `completion-ml-ranking`"
    This reverts commit 30d37ec9
    
    GitOrigin-RevId: 342bab28ad920d0cc40e688ab98f832af5905d87

--- idea/src/org/jetbrains/kotlin/idea/configuration/MLCompletionForKotlin.kt
+++ idea/src/org/jetbrains/kotlin/idea/configuration/MLCompletionForKotlin.kt
@@ -5,8 +5,9 @@
 
 package org.jetbrains.kotlin.idea.configuration
 
-import com.intellij.completion.ml.settings.CompletionMLRankingSettings
+import com.intellij.completion.settings.CompletionMLRankingSettings
 import org.jetbrains.kotlin.idea.KotlinBundle
+import kotlin.reflect.jvm.isAccessible
 
 object MLCompletionForKotlinFeature : ExperimentalFeature() {
     override val title: String
@@ -27,14 +28,28 @@ internal object MLCompletionForKotlin {
     var isEnabled: Boolean
         get() {
             val settings = CompletionMLRankingSettings.getInstance()
-            return settings.isRankingEnabled && settings.isLanguageEnabled("Kotlin")
+            return settings.isRankingEnabledHacked && settings.isLanguageEnabled("Kotlin")
         }
         set(value) {
             val settings = CompletionMLRankingSettings.getInstance()
-            if (value && !settings.isRankingEnabled) {
-                settings.isRankingEnabled = true
+            if (value && !settings.isRankingEnabledHacked) {
+                settings.isRankingEnabledHacked = true
             }
 
             settings.setLanguageEnabled("Kotlin", value)
         }
 }
+
+/**
+ * Setter for this property is package-private, so we have to use reflection to change its value.
+ */
+private var CompletionMLRankingSettings.isRankingEnabledHacked: Boolean
+    get() = isRankingEnabled
+    set(value) {
+        val rankingEnabledSetter = this::class.members.find { it.name == "setRankingEnabled" }
+            ?: error("Cannot find 'setRankingEnabled' in class members: ${this::class.members}")
+
+        rankingEnabledSetter.isAccessible = true
+        rankingEnabledSetter.call(this, value)
+        rankingEnabledSetter.isAccessible = false
+    }
