3e9fc0be782edb17234f932bbf6c8541fea0a5f3
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2020-10-29 00:24:21 +0900

    Revert "SSR: Add structuralsearch package"
    This reverts commit b8d847a7
    
    GitOrigin-RevId: 42bbffdced29b9ab6e9a0e52bf3ceec14fe40f90

--- idea/test/org/jetbrains/kotlin/idea/structuralsearch/search/KotlinSSThrowExpressionTest.kt
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.kotlin.idea.structuralsearch.search
-
-import org.jetbrains.kotlin.idea.structuralsearch.KotlinSSResourceInspectionTest
-
-class KotlinSSThrowExpressionTest : KotlinSSResourceInspectionTest() {
-    override fun getBasePath(): String = "throwExpression"
-
-    fun testAnyException() { doTest("throw '_") }
-}
