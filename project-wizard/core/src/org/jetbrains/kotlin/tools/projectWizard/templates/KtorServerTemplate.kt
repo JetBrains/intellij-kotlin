3e68f1e41cbba01581cf89644a1c6f8f9983f61f
Alexander Udalov <alexander.udalov@jetbrains.com>
2020-11-17 20:26:19 +0100

    Capitalize/decapitalize only ASCII characters across project
    Use {de,}capitalizeAsciiOnly and to{Lower,Upper}CaseAsciiOnly where
    possible, and stdlib's functions with Locale.US everywhere else.
    
    Otherwise, if the default system locale is Turkish, the capital latin
    letter "I" is transformed in toLowerCase to "ı" (see
    https://github.com/JetBrains/kotlin/blob/66bc142f92085047a1ca64f9a291f0496e33dd98/libraries/stdlib/jvm/test/text/StringJVMTest.kt#L119),
    which for example breaks the codegen for `intArrayOf` in
    KT-25400/KT-43405.
    
    Similarly, lower case latin letter "i" is transformed to "İ".
    
     #KT-13631 Fixed
     #KT-25400 Fixed
     #KT-43405 Fixed
    
    Original commit: 77a9d14f93a8a0581f276bee3a1e211781791aee
    
    GitOrigin-RevId: 78025c78d51c98f0fe38f959afc10ad1f843abec

--- project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/templates/KtorServerTemplate.kt
+++ project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/templates/KtorServerTemplate.kt
@@ -7,6 +7,11 @@ package org.jetbrains.kotlin.tools.projectWizard.templates
 
 
 import org.jetbrains.annotations.NonNls
+import org.jetbrains.kotlin.tools.projectWizard.KotlinNewProjectWizardBundle
+import org.jetbrains.kotlin.tools.projectWizard.Versions
+import org.jetbrains.kotlin.tools.projectWizard.WizardGradleRunConfiguration
+import org.jetbrains.kotlin.tools.projectWizard.WizardRunConfiguration
+import org.jetbrains.kotlin.tools.projectWizard.core.Reader
 import org.jetbrains.kotlin.tools.projectWizard.core.Writer
 import org.jetbrains.kotlin.tools.projectWizard.core.asPath
 import org.jetbrains.kotlin.tools.projectWizard.core.buildList
@@ -15,16 +20,17 @@ import org.jetbrains.kotlin.tools.projectWizard.ir.buildsystem.*
 import org.jetbrains.kotlin.tools.projectWizard.ir.buildsystem.gradle.multiplatform.TargetConfigurationIR
 import org.jetbrains.kotlin.tools.projectWizard.ir.buildsystem.gradle.multiplatform.addWithJavaIntoJvmTarget
 import org.jetbrains.kotlin.tools.projectWizard.library.MavenArtifact
-import org.jetbrains.kotlin.tools.projectWizard.plugins.kotlin.ModuleType
-import org.jetbrains.kotlin.tools.projectWizard.transformers.interceptors.InterceptionPoint
-import org.jetbrains.kotlin.tools.projectWizard.KotlinNewProjectWizardBundle
-import org.jetbrains.kotlin.tools.projectWizard.Versions
-import org.jetbrains.kotlin.tools.projectWizard.WizardGradleRunConfiguration
-import org.jetbrains.kotlin.tools.projectWizard.WizardRunConfiguration
-import org.jetbrains.kotlin.tools.projectWizard.core.Reader
 import org.jetbrains.kotlin.tools.projectWizard.moduleConfigurators.moduleType
+import org.jetbrains.kotlin.tools.projectWizard.phases.GenerationPhase
+import org.jetbrains.kotlin.tools.projectWizard.plugins.kotlin.ModuleType
 import org.jetbrains.kotlin.tools.projectWizard.plugins.kotlin.ProjectKind
-import org.jetbrains.kotlin.tools.projectWizard.settings.buildsystem.*
+import org.jetbrains.kotlin.tools.projectWizard.settings.DisplayableSettingItem
+import org.jetbrains.kotlin.tools.projectWizard.settings.buildsystem.DefaultRepository
+import org.jetbrains.kotlin.tools.projectWizard.settings.buildsystem.Module
+import org.jetbrains.kotlin.tools.projectWizard.settings.buildsystem.Repositories
+import org.jetbrains.kotlin.tools.projectWizard.settings.buildsystem.SourcesetType
+import org.jetbrains.kotlin.tools.projectWizard.transformers.interceptors.InterceptionPoint
+import java.util.*
 
 class KtorServerTemplate : Template() {
     override val title: String = KotlinNewProjectWizardBundle.message("module.template.ktor.server.title")
@@ -52,21 +58,7 @@ class KtorServerTemplate : Template() {
     override fun Writer.getIrsToAddToBuildFile(module: ModuleIR): List<BuildSystemIR> = buildList {
         +RepositoryIR(Repositories.KTOR)
         +RepositoryIR(DefaultRepository.JCENTER)
-        +runTaskIrs(mainClass = "ServerKt")
-    }
-
-    override fun Reader.createRunConfigurations(module: ModuleIR): List<WizardRunConfiguration> = buildList {
-        +WizardGradleRunConfiguration("Run", "run", emptyList())
-    }
-
-    override fun updateTargetIr(module: ModuleIR, targetConfigurationIR: TargetConfigurationIR): TargetConfigurationIR =
-        targetConfigurationIR.addWithJavaIntoJvmTarget()
-
-    override fun Reader.getFileTemplates(module: ModuleIR): List<FileTemplateDescriptorWithPath> = listOf(
-        FileTemplateDescriptor("$id/server.kt.vm", "server.kt".asPath()) asSrcOf SourcesetType.main
-    )
-
-    val imports = InterceptionPoint("imports", emptyList<String>())
+        +runTaskIrs(mainClass =|||||||Point("imports", emptyList<String>())
     val routes = InterceptionPoint("routes", emptyList<String>())
     val elements = InterceptionPoint("elements", emptyList<String>())
 
