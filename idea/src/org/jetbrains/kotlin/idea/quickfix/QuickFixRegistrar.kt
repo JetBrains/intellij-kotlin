090feb7845bfdb367b0da41c1923dbb47d1115be
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2020-12-22 18:24:14 +0100

    KT-44043 [Sealed interfaces]: quickfix for top level abstractions
    Original commit: 521bebee0ff52fc033f18f0e2a377e7adddbd20f
    
    GitOrigin-RevId: 4e492d2ec8f00ca4ab4e2e527336c3dbf7d8caeb

--- idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
+++ idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
@@ -677,5 +677,8 @@ class QuickFixRegistrar : QuickFixContributor {
         COMPATIBILITY_WARNING.registerFactory(UseFullyQualifiedCallFix)
 
         INLINE_CLASS_CONSTRUCTOR_NOT_FINAL_READ_ONLY_PARAMETER.registerFactory(InlineClassConstructorNotValParameterFactory)
+
+        SEALED_INHERITOR_IN_DIFFERENT_PACKAGE.registerFactory(MoveToSealedMatchingPackageFix)
+        SEALED_INHERITOR_IN_DIFFERENT_MODULE.registerFactory(MoveToSealedMatchingPackageFix)
     }
 }
