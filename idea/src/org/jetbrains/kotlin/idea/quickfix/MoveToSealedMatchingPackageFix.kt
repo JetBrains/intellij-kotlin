21eb77fdb2d668441c671c8f063fb44dc3988035
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2021-01-04 11:29:10 +0100

    KT-44043 [Sealed interfaces]: tests
    So far, quick-fix tests infrastructure neither supports the check
    for multiple files (several .before/.after pairs) nor multi module
    structure. Provided tests are quite limited yet allow to make sure that
    the fix in question appears in expected context and get activated.
    
    ^KT-44043 fixed
    
    (cherry picked from commit eed27906e30169f80a3040aca6aabfbca2f55907)
    
    Original commit: be981a8f582d57c9e500764e3d53d6bdef8a0c2e
    
    (cherry picked from commit f9f1294bca83a4acab730ef0df9539dc4d24a08c)
    
    GitOrigin-RevId: e236467eae3c0527d378151df27a6a4fe566d264

--- idea/src/org/jetbrains/kotlin/idea/quickfix/MoveToSealedMatchingPackageFix.kt
+++ idea/src/org/jetbrains/kotlin/idea/quickfix/MoveToSealedMatchingPackageFix.kt
@@ -7,26 +7,47 @@ package org.jetbrains.kotlin.idea.quickfix
 
 import com.intellij.openapi.actionSystem.LangDataKeys
 import com.intellij.openapi.actionSystem.impl.SimpleDataContext
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFileSystemItem
 import com.intellij.psi.util.parentOfType
+import com.intellij.refactoring.PackageWrapper
+import com.intellij.refactoring.move.MoveCallback
+import com.intellij.refactoring.move.MoveHandler
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.descriptors.isSealed
 import org.jetbrains.kotlin.diagnostics.Diagnostic
 import org.jetbrains.kotlin.idea.KotlinBundle
+import org.jetbrains.kotlin.idea.actions.internal.refactoringTesting.cases.*
+import org.jetbrains.kotlin.idea.refactoring.move.getTargetPackageFqName
+import org.jetbrains.kotlin.idea.refactoring.move.guessNewFileName
 import org.jetbrains.kotlin.idea.caches.resolve.resolveToDescriptorIfAny
 import org.jetbrains.kotlin.idea.refactoring.move.moveDeclarations.MoveKotlinDeclarationsHandler
+import org.jetbrains.kotlin.idea.refactoring.move.moveDeclarations.MoveKotlinDeclarationsHandlerActions
+import org.jetbrains.kotlin.idea.refactoring.move.moveDeclarations.ui.KotlinAwareMoveFilesOrDirectoriesModel
+import org.jetbrains.kotlin.idea.refactoring.move.moveDeclarations.ui.MoveKotlinNestedClassesToUpperLevelModel
+import org.jetbrains.kotlin.idea.refactoring.move.moveDeclarations.ui.MoveKotlinTopLevelDeclarationsModel
 import org.jetbrains.kotlin.idea.references.resolveMainReferenceToDescriptors
+import org.jetbrains.kotlin.idea.util.application.executeCommand
 import org.jetbrains.kotlin.js.resolve.diagnostics.findPsi
 import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.containingClassOrObject
 import org.jetbrains.kotlin.resolve.descriptorUtil.getSuperClassNotAny
 import org.jetbrains.kotlin.resolve.descriptorUtil.getSuperInterfaces
 import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedClassDescriptor
 
 class MoveToSealedMatchingPackageFix(element: KtTypeReference) : KotlinQuickFixAction<KtTypeReference>(element) {
 
-    private val moveHandler = MoveKotlinDeclarationsHandler(false)
+    private val moveHandler =
+        if (ApplicationManager.getApplication().isUnitTestMode) {
+            MoveKotlinDeclarationsHandler(MoveKotlinDeclarationsHandlerTestActions())
+        } else {
+            MoveKotlinDeclarationsHandler(false)
+        }
 
     override fun invoke(project: Project, editor: Editor?, file: KtFile) {
         val typeReference = element ?: return
@@ -79,4 +100,115 @@ class MoveToSealedMatchingPackageFix(element: KtTypeReference) : KotlinQuickFixA
             return MoveToSealedMatchingPackageFix(typeReference)
         }
     }
+}
+
+private class MoveKotlinDeclarationsHandlerTestActions : MoveKotlinDeclarationsHandlerActions {
+
+    override fun invokeMoveKotlinTopLevelDeclarationsRefactoring(
+        project: Project,
+        elementsToMove: Set<KtNamedDeclaration>,
+        targetPackageName: String,
+        targetDirectory: PsiDirectory?,
+        targetFile: KtFile?,
+        freezeTargets: Boolean,
+        moveToPackage: Boolean,
+        moveCallback: MoveCallback?
+    ) {
+        val sourceFiles = getSourceFiles(elementsToMove)
+        val targetFilePath =
+            targetFile?.virtualFile?.path ?: sourceFiles[0].virtualFile.parent.path + "/" + guessNewFileName(elementsToMove)
+
+        val model = MoveKotlinTopLevelDeclarationsModel(
+            project = project,
+            elementsToMove = elementsToMove.toList(),
+            targetPackage = targetPackageName,
+            selectedPsiDirectory = targetDirectory,
+            fileNameInPackage = "Derived.kt",
+            targetFilePath = targetFilePath,
+            isMoveToPackage = true,
+            isSearchReferences = false,
+            isSearchInComments = false,
+            isSearchInNonJavaFiles = false,
+            isDeleteEmptyFiles = false,
+            applyMPPDeclarations = false,
+            moveCallback = null
+        )
+
+        model.computeModelResult(throwOnConflicts = true).processor.run()
+    }
+
+    private fun getSourceFiles(elementsToMove: Collection<KtNamedDeclaration>): List<KtFile> {
+        return elementsToMove.map { obj: KtPureElement -> obj.containingKtFile }
+            .distinct()
+    }
+
+    override fun invokeKotlinSelectNestedClassChooser(nestedClass: KtClassOrObject, targetContainer: PsiElement?) =
+        doWithMoveKotlinNestedClassesToUpperLevelModel(nestedClass, targetContainer)
+
+    private fun doWithMoveKotlinNestedClassesToUpperLevelModel(nestedClass: KtClassOrObject, targetContainer: PsiElement?) {
+
+        val outerClass = nestedClass.containingClassOrObject ?: throw FailedToRunCaseException()
+        val newTarget = targetContainer
+            ?: outerClass.containingClassOrObject
+            ?: outerClass.containingFile.let { it.containingDirectory ?: it }
+
+        val packageName = getTargetPackageFqName(newTarget)?.asString() ?: ""
+
+        val model = object : MoveKotlinNestedClassesToUpperLevelModel(
+            project = nestedClass.project,
+            innerClass = nestedClass,
+            target = newTarget,
+            parameter = "",
+            className = nestedClass.name ?: "",
+            passOuterClass = false,
+            searchInComments = false,
+            isSearchInNonJavaFiles = false,
+            packageName = packageName,
+            isOpenInEditor = false
+        ) {
+            override fun chooseSourceRoot(
+                newPackage: PackageWrapper,
+                contentSourceRoots: List<VirtualFile>,
+                initialDir: PsiDirectory?
+            ) = contentSourceRoots.firstOrNull()
+        }
+
+        model.computeModelResult(throwOnConflicts = true).processor.run()
+    }
+
+    override fun invokeKotlinAwareMoveFilesOrDirectoriesRefactoring(
+        project: Project,
+        initialDirectory: PsiDirectory?,
+        elements: List<PsiFileSystemItem>,
+        moveCallback: MoveCallback?
+    ) {
+        val targetPath =
+            initialDirectory?.virtualFile?.path
+                ?: elements.firstOrNull()?.containingFile?.virtualFile?.path
+                ?: throw NotImplementedError()
+
+        val model = KotlinAwareMoveFilesOrDirectoriesModel(
+            project = project,
+            elementsToMove = elements,
+            targetDirectoryName = randomDirectoryPathMutator(targetPath),
+            updatePackageDirective = randomBoolean(),
+            searchReferences = randomBoolean(),
+            moveCallback = null
+        )
+
+        project.executeCommand(MoveHandler.getRefactoringName()) {
+            model.computeModelResult().processor.run()
+        }
+    }
+
+    override fun showErrorHint(project: Project, editor: Editor?, message: String, title: String, helpId: String?) =
+        throw NotImplementedError()
+
+    override fun invokeMoveKotlinNestedClassesRefactoring(
+        project: Project,
+        elementsToMove: List<KtClassOrObject>,
+        originalClass: KtClassOrObject,
+        targetClass: KtClassOrObject,
+        moveCallback: MoveCallback?
+    ) = throw NotImplementedError()
 }
