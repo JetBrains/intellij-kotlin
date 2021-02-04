SHKD13/kotlin

Kotlin highlight passes are reworked
Relates to #KT-37702

idea/idea-analysis/src/org/jetbrains/kotlin/idea/highlighter/DebugInfoHighlightingPass.kt
@@ -1,0 +1,100 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.idea.highlighter
+
+import com.intellij.codeHighlighting.*
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.progress.ProcessCanceledException
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import org.jetbrains.kotlin.checkers.utils.DebugInfoUtil
+import org.jetbrains.kotlin.idea.KotlinPluginUtil
+import org.jetbrains.kotlin.idea.util.ProjectRootsUtil
+import org.jetbrains.kotlin.idea.util.application.isInternal
+import org.jetbrains.kotlin.psi.KtCodeFragment
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtReferenceExpression
+
+class DebugInfoHighlightingPass(file: KtFile, document: Document) : AbstractKotlinHighlightingPass(file, document) {
+    override val annotator: Annotator
+        get() = DebugInfoAnnotator()
+
+    private inner class DebugInfoAnnotator : Annotator {
+        override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+            if (element is KtFile && element !is KtCodeFragment) {
+                fun errorAnnotation(
+                    expression: PsiElement,
+                    message: String,
+                    textAttributes: TextAttributesKey? = KotlinHighlightingColors.DEBUG_INFO
+                ) =
+                    holder.newAnnotation(HighlightSeverity.ERROR, "[DEBUG] $message")
+                        .range(expression.textRange)
+                        .also {
+                            textAttributes?.let { ta -> it.textAttributes(ta) }
+                        }
+                        .create()
+
+                try {
+                    DebugInfoUtil.markDebugAnnotations(element, bindingContext(), object : DebugInfoUtil.DebugInfoReporter() {
+                        override fun reportElementWithErrorType(expression: KtReferenceExpression) =
+                            errorAnnotation(expression, "Resolved to error element", KotlinHighlightingColors.RESOLVED_TO_ERROR)
+
+                        override fun reportMissingUnresolved(expression: KtReferenceExpression) =
+                            errorAnnotation(
+                                expression,
+                                "Reference is not resolved to anything, but is not marked unresolved"
+                            )
+
+                        override fun reportUnresolvedWithTarget(expression: KtReferenceExpression, target: String) =
+                            errorAnnotation(
+                                expression,
+                                "Reference marked as unresolved is actually resolved to $target"
+                            )
+                    })
+                } catch (e: ProcessCanceledException) {
+                    throw e
+                } catch (e: Throwable) {
+                    // TODO
+                    errorAnnotation(element, e.javaClass.canonicalName + ": " + e.message, null)
+                    e.printStackTrace()
+                }
+
+            }
+        }
+    }
+
+    class Factory : TextEditorHighlightingPassFactory {
+        override fun createHighlightingPass(file: PsiFile, editor: Editor): TextEditorHighlightingPass? {
+            return if (file is KtFile &&
+                (isInternal() && (KotlinPluginUtil.isSnapshotVersion() || KotlinPluginUtil.isDevVersion())) &&
+                ProjectRootsUtil.isInProjectOrLibSource(file)
+            ) {
+                DebugInfoHighlightingPass(file, editor.document)
+            } else {
+                null
+            }
+        }
+    }
+
+    class Registrar : TextEditorHighlightingPassFactoryRegistrar {
+        override fun registerHighlightingPassFactory(registrar: TextEditorHighlightingPassRegistrar, project: Project) {
+            registrar.registerTextEditorHighlightingPass(
+                Factory(),
+                intArrayOf(Pass.UPDATE_ALL),
+                null,
+                false,
+                -1
+            )
+        }
+    }
+
+}

idea/idea-frontend-independent/src/org/jetbrains/kotlin/idea/util/ApplicationUtils.kt
@@ -58,6 +58,8 @@ inline fun invokeLater(expired: Condition<*>, crossinline action: () -> Unit) =
 
 inline fun isUnitTestMode(): Boolean = ApplicationManager.getApplication().isUnitTestMode
 
+inline fun isInternal(): Boolean = ApplicationManager.getApplication().isInternal
+
 inline fun <reified T : Any> ComponentManager.getServiceSafe(): T =
     this.getService(T::class.java) ?: error("Unable to locate service ${T::class.java.name}")
 

JetBrains/intellij-kotlin

^KT-37702 Fixed
(cherry picked from commit 913c298be858b63e472cfd6c58af11702b3a101d)
Original commit: ac1d975fd393a071bae65e38e239d024663f8e68
GitOrigin-RevId: b0f9cc30f4821be3eecf04ed65a3450b8ed716af

analysis/src/org/jetbrains/kotlin/idea/highlighter/DebugInfoHighlightingPass.kt
@@ -1,0 +1,100 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.idea.highlighter
+
+import com.intellij.codeHighlighting.*
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.progress.ProcessCanceledException
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import org.jetbrains.kotlin.checkers.utils.DebugInfoUtil
+import org.jetbrains.kotlin.idea.KotlinPluginUtil
+import org.jetbrains.kotlin.idea.util.ProjectRootsUtil
+import org.jetbrains.kotlin.idea.util.application.isApplicationInternalMode
+import org.jetbrains.kotlin.psi.KtCodeFragment
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtReferenceExpression
+
+class DebugInfoHighlightingPass(file: KtFile, document: Document) : AbstractBindingContextAwareHighlightingPassBase(file, document) {
+    override val annotator: Annotator
+        get() = DebugInfoAnnotator()
+
+    private inner class DebugInfoAnnotator : Annotator {
+        override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+            if (element is KtFile && element !is KtCodeFragment) {
+                fun errorAnnotation(
+                    expression: PsiElement,
+                    message: String,
+                    textAttributes: TextAttributesKey? = KotlinHighlightingColors.DEBUG_INFO
+                ) =
+                    holder.newAnnotation(HighlightSeverity.ERROR, "[DEBUG] $message")
+                        .range(expression.textRange)
+                        .also {
+                            textAttributes?.let { ta -> it.textAttributes(ta) }
+                        }
+                        .create()
+
+                try {
+                    DebugInfoUtil.markDebugAnnotations(element, bindingContext(), object : DebugInfoUtil.DebugInfoReporter() {
+                        override fun reportElementWithErrorType(expression: KtReferenceExpression) =
+                            errorAnnotation(expression, "Resolved to error element", KotlinHighlightingColors.RESOLVED_TO_ERROR)
+
+                        override fun reportMissingUnresolved(expression: KtReferenceExpression) =
+                            errorAnnotation(
+                                expression,
+                                "Reference is not resolved to anything, but is not marked unresolved"
+                            )
+
+                        override fun reportUnresolvedWithTarget(expression: KtReferenceExpression, target: String) =
+                            errorAnnotation(
+                                expression,
+                                "Reference marked as unresolved is actually resolved to $target"
+                            )
+                    })
+                } catch (e: ProcessCanceledException) {
+                    throw e
+                } catch (e: Throwable) {
+                    // TODO
+                    errorAnnotation(element, e.javaClass.canonicalName + ": " + e.message, null)
+                    e.printStackTrace()
+                }
+
+            }
+        }
+    }
+
+    class Factory : TextEditorHighlightingPassFactory {
+        override fun createHighlightingPass(file: PsiFile, editor: Editor): TextEditorHighlightingPass? {
+            return if (file is KtFile &&
+                (isApplicationInternalMode() && (KotlinPluginUtil.isSnapshotVersion() || KotlinPluginUtil.isDevVersion())) &&
+                ProjectRootsUtil.isInProjectOrLibSource(file)
+            ) {
+                DebugInfoHighlightingPass(file, editor.document)
+            } else {
+                null
+            }
+        }
+    }
+
+    class Registrar : TextEditorHighlightingPassFactoryRegistrar {
+        override fun registerHighlightingPassFactory(registrar: TextEditorHighlightingPassRegistrar, project: Project) {
+            registrar.registerTextEditorHighlightingPass(
+                Factory(),
+                /* runAfterCompletionOf = */ intArrayOf(Pass.UPDATE_ALL),
+                /* runAfterStartingOf = */ null,
+                /* runIntentionsPassAfter = */ false,
+                /* forcedPassId = */ -1
+            )
+        }
+    }
+
+}

frontend-independent/src/org/jetbrains/kotlin/idea/util/ApplicationUtils.kt
@@ -58,6 +58,8 @@ inline fun invokeLater(expired: Condition<*>, crossinline action: () -> Unit) =
 
 inline fun isUnitTestMode(): Boolean = ApplicationManager.getApplication().isUnitTestMode
 
+inline fun isApplicationInternalMode(): Boolean = ApplicationManager.getApplication().isInternal
+
 inline fun <reified T : Any> ComponentManager.getServiceSafe(): T =
     this.getService(T::class.java) ?: error("Unable to locate service ${T::class.java.name}")
 