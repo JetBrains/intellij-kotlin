46f28c804eef71647369c00ee8e6efe3237bcb2e
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2021-02-09 16:42:43 +0300

    [wizard, Kotlin/JS] Update tests from `output.js` into js.js
    ^KTIJ-1201 fixed
    
    (cherry picked from commit 42c12962b51e039e6a4533b8c058dd17afd59a01)
    
    GitOrigin-RevId: 8a4766bd33cf0c3c99138d50cdf43cb93f5d3c58

--- project-wizard/cli/testData/projectTemplatesBuildFileGeneration/fullStackWebApplication/build.gradle.kts
+++ project-wizard/cli/testData/projectTemplatesBuildFileGeneration/fullStackWebApplication/build.gradle.kts
@@ -83,7 +83,7 @@ application {
 }
 
 tasks.getByName<KotlinWebpack>("jsBrowserProductionWebpack") {
-    outputFileName = "output.js"
+    outputFileName = "js.js"
 }
 
 tasks.getByName<Jar>("jvmJar") {
