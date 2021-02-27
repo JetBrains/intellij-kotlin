98d6f9b5d97af13d3729cd4be4b97944589c647e
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2021-02-27 19:32:00 +0100

    [FIR]: sealed hierarchy processor for IDE
    From now on sealed declarations get resolved with the help of
    FirIdeSealedHierarchyProcessor. This change entails correct IDE side
    check for sealed when exhaustiveness.
    
    Original commit: e6ccdff2f79e4c6fc5a60c32c12778bcc21d4a1e
    
    GitOrigin-RevId: c3e6336f134898f22340b9bf7d11518e3477feac

--- generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -64,9 +64,7 @@ import org.jetbrains.kotlin.idea.editor.quickDoc.AbstractQuickDocProviderTest
 import org.jetbrains.kotlin.idea.externalAnnotations.AbstractExternalAnnotationTest
 import org.jetbrains.kotlin.idea.filters.AbstractKotlinExceptionFilterTest
 import org.jetbrains.kotlin.idea.fir.AbstractKtDeclarationAndFirDeclarationEqualityChecker
-import org.jetbrains.kotlin.idea.fir.low.level.api.AbstractFirLazyDeclarationResolveTest
-import org.jetbrains.kotlin.idea.fir.low.level.api.AbstractFirLazyResolveTest
-import org.jetbrains.kotlin.idea.fir.low.level.api.AbstractFirMultiModuleLazyResolveTest
+import org.jetbrains.kotlin.idea.fir.low.level.api.*
 import org.jetbrains.kotlin.idea.fir.low.level.api.file.structure.AbstractFileStructureAndOutOfBlockModificationTrackerConsistencyTest
 import org.jetbrains.kotlin.idea.fir.low.level.api.file.structure.AbstractFileStructureTest
 import org.jetbrains.kotlin.idea.fir.low.level.api.sessions.AbstractSessionsInvalidationTest
@@ -1021,6 +1019,9 @@ private fun assembleWorkspace(): TWorkspace = workspace {
     testGroup("fir-low-level-api", testDataPath = "testdata") {
         testClass<AbstractFirMultiModuleLazyResolveTest> {
             model("multiModuleLazyResolve", pattern = DIRECTORY, isRecursive = false)
+        }
+        testClass<AbstractFirSealedInheritorsTest> {
+            model("resolveSealed", recursive = false, extension = null)
         }
         testClass<AbstractFirLazyDeclarationResolveTest> {
             model("lazyResolve")
