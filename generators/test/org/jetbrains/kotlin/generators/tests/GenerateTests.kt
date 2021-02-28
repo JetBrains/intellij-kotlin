riyasvmc/kotlin

JVM IR: Enable evaluate expression tests for the JVM IR backend

generators/tests/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -249,6 +249,11 @@ fun main(args: Array<String>) {
                 model("evaluation/multipleBreakpoints", testMethod = "doMultipleBreakpointsTest")
             }
 
+            testClass<AbstractIrKotlinEvaluateExpressionTest> {
+                model("evaluation/singleBreakpoint", testMethod = "doSingleBreakpointTest", targetBackend = TargetBackend.JVM_IR)
+                model("evaluation/multipleBreakpoints", testMethod = "doMultipleBreakpointsTest", targetBackend = TargetBackend.JVM_IR)
+            }
+
             testClass<AbstractSelectExpressionForDebuggerTest> {
                 model("selectExpression", recursive = false)
                 model("selectExpression/disallowMethodCalls", testMethod = "doTestWoMethodCalls")

JetBrains/intellij-kotlin

Original commit: 48b736e551b543cc6c877c5cb83bccb56d28b4fa
(cherry picked from commit 890a65964a471188b08565129eca8cf4d5e42b58)
GitOrigin-RevId: 2ed761f292f80e634d2e986f3515e027ee4eb36b

generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -202,6 +202,11 @@ private fun assembleWorkspace(): TWorkspace = workspace {
             model("evaluation/multipleBreakpoints", testMethodName = "doMultipleBreakpointsTest")
         }
 
+        testClass<AbstractIrKotlinEvaluateExpressionTest> {
+            model("evaluation/singleBreakpoint", testMethodName = "doSingleBreakpointTest", targetBackend = TargetBackend.JVM_IR)
+            model("evaluation/multipleBreakpoints", testMethodName = "doMultipleBreakpointsTest", targetBackend = TargetBackend.JVM_IR)
+        }
+
         testClass<AbstractSelectExpressionForDebuggerTest> {
             model("selectExpression", isRecursive = false)
             model("selectExpression/disallowMethodCalls", testMethodName = "doTestWoMethodCalls")