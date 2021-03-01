82c4bc6291374f0db3607774f80f220d0bab2ab7
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-03-01 19:12:17 +0100

    Wizard: migrate compose projects to 0.3.1
    GitOrigin-RevId: 46b334944050b4c432c88a10537936146a567d01

--- project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/templates/compose/ComposeJvmDesktopTemplate.kt
+++ project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/templates/compose/ComposeJvmDesktopTemplate.kt
@@ -55,6 +55,7 @@ class ComposeJvmDesktopTemplate : Template() {
                 "nativeDistributions" {
                     "targetFormats"(raw("TargetFormat.Dmg"), raw("TargetFormat.Msi"), raw("TargetFormat.Deb"))
                     "packageName" assign const(module.name)
+                    "packageVersion" assign const("1.0.0")
                 }
             }
         }
