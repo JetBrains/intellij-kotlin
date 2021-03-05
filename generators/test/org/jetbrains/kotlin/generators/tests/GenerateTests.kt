b2898afb533f6b2fc7431575c3f1548f20886374
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2021-03-05 16:37:47 +0100

    Revert "[FIR]: sealed hierarchy processor for IDE"
    This reverts commit e6ccdff2
    
    Original commit: a0b901b23a0c329e04d0360a1e1ece1366ba6c43
    
    GitOrigin-RevId: c6f073eb728d7e5c560c84cb6755b1407ae6707c

--- generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1021,9 +1021,6 @@ private fun assembleWorkspace(): TWorkspace = workspace {
         testClass<AbstractFirMultiModuleLazyResolveTest> {
             model("multiModuleLazyResolve", pattern = DIRECTORY, isRecursive = false)
         }
-        testClass<AbstractFirSealedInheritorsTest> {
-            model("resolveSealed", recursive = false, extension = null)
-        }
         testClass<AbstractFirLazyDeclarationResolveTest> {
             model("lazyResolve")
         }
