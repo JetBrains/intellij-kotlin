8709f67b28602f424966fd9747f7fe92f17e291f
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-03-16 14:18:32 +0100

    Rewrite AbstractDiagnosticMessageTest so it uses ordinary IDE-specific resolution mechanisms
    This commit fixes the following tests:
    - DiagnosticMessageTestGenerated.testAnnotationsForResolve()
    - DiagnosticMessageTestGenerated.testNoneApplicableCallableReference()
    
    (cherry picked from commit 53e9676683d8fcdc3baba2d608fab98f8eb46100)
    
    GitOrigin-RevId: 2dacdbc7fae756fa5aa71037f5f0ceca6a33f814

--- analysis/src/org/jetbrains/kotlin/idea/project/Platform.kt
+++ analysis/src/org/jetbrains/kotlin/idea/project/Platform.kt
@@ -161,7 +161,7 @@ val Module.languageVersionSettings: LanguageVersionSettings
     }
 
 @TestOnly
-fun Module.withLanguageVersionSettings(value: LanguageVersionSettings, body: () -> Unit) {
+fun <T> Module.withLanguageVersionSettings(value: LanguageVersionSettings, body: () -> T): T {
     val previousLanguageVersionSettings = getUserData(LANGUAGE_VERSION_SETTINGS)
     try {
         putUserData(
@@ -176,7 +176,7 @@ fun Module.withLanguageVersionSettings(value: LanguageVersionSettings, body: () 
             )
         )
 
-        body()
+        return body()
     } finally {
         putUserData(LANGUAGE_VERSION_SETTINGS, previousLanguageVersionSettings)
     }
