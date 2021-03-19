884ea3385d50926209a1edbdee05b26562bd9c88
Victor Petukhov <victor.petukhov@jetbrains.com>
2021-03-19 15:14:22 +0300

    Don't lose inference session in all the possible locations
    Original commit: 80ac62864d0ee85d1f57f869d652fbe66d6fc00a
    
    GitOrigin-RevId: 7240df398f78b6abac259d0be4226baea4b063ce

--- analysis/src/org/jetbrains/kotlin/idea/project/ResolveElementCache.kt
+++ analysis/src/org/jetbrains/kotlin/idea/project/ResolveElementCache.kt
@@ -600,7 +600,8 @@ class ResolveElementCache(
             descriptor,
             descriptor.unsubstitutedPrimaryConstructor,
             descriptor.scopeForConstructorHeaderResolution,
-            descriptor.scopeForMemberDeclarationResolution
+            descriptor.scopeForMemberDeclarationResolution,
+            resolveSession.inferenceSession
         )
 
         return trace
@@ -724,7 +725,8 @@ class ResolveElementCache(
                 trace,
                 primaryConstructor,
                 constructorDescriptor,
-                scope
+                scope,
+                resolveSession.inferenceSession
             )
 
             forceResolveAnnotationsInside(primaryConstructor)
@@ -754,7 +756,9 @@ class ResolveElementCache(
         val classOrObjectDescriptor = resolveSession.resolveToDescriptor(anonymousInitializer.containingDeclaration) as LazyClassDescriptor
 
         val bodyResolver = createBodyResolver(resolveSession, trace, file, statementFilter)
-        bodyResolver.resolveAnonymousInitializer(DataFlowInfo.EMPTY, anonymousInitializer, classOrObjectDescriptor)
+        bodyResolver.resolveAnonymousInitializer(
+            DataFlowInfo.EMPTY, anonymousInitializer, classOrObjectDescriptor, resolveSession.inferenceSession
+        )
 
         forceResolveAnnotationsInside(anonymousInitializer)
 
