zsnake1209/kotlin

Support compiler flag -Xcodeanalysis-annotations

idea/idea-analysis/src/org/jetbrains/kotlin/idea/compiler/IDELanguageSettingsProvider.kt
@@ -67,7 +67,8 @@ object IDELanguageSettingsProvider : LanguageSettingsProvider {
 
             result = JavaTypeEnhancementStateParser(MessageCollector.NONE).parse(
                 compilerArguments.jsr305,
-                compilerArguments.supportCompatqualCheckerFrameworkAnnotations
+                compilerArguments.supportCompatqualCheckerFrameworkAnnotations,
+                compilerArguments.codeAnalysisState
             )
 
         }

JetBrains/intellij-kotlin

Original commit: f3a490ee16f2154cb05c6bb503324f797c80188d
(cherry picked from commit 1afa5037b42200b07d3614640bff507dc111a2fb)
GitOrigin-RevId: e78117032f435f05a7757408cd138776a14b2abf

analysis/src/org/jetbrains/kotlin/idea/compiler/IDELanguageSettingsProvider.kt
@@ -79,7 +79,8 @@ object IDELanguageSettingsProvider : LanguageSettingsProvider {
 
             result = JavaTypeEnhancementStateParser(MessageCollector.NONE).parse(
                 compilerArguments.jsr305,
-                compilerArguments.supportCompatqualCheckerFrameworkAnnotations
+                compilerArguments.supportCompatqualCheckerFrameworkAnnotations,
+                compilerArguments.jspecifyAnnotations
             )
 
         }