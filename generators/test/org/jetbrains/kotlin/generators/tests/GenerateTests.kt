d5932bdd50f2bf235abc6dea76238bb326cf8d4a
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-04-01 14:31:20 +0200

    Extract kotlin.j2k.new tests into separate module
    This commit is required to split kotlin.j2k.new module into smaller modules for more
    precise dependency configurations in order to get rid of circular dependencies
    with kotlin.j2k.new module
    
    Before this commit:
    ```
    Project kotlin-ide: there is circular dependency between tests of 'kotlin.j2k.old' module,
    tests of 'kotlin.maven' module, tests of 'kotlin.gradle.gradle-tooling' module,
    tests of 'kotlin.test-framework' module, tests of 'kotlin.native' module,
    tests of 'kotlin.j2k.new' module, tests of 'kotlin.idea.tests' module,
    tests of 'kotlin.uast.uast-kotlin-idea' module, tests of 'kotlin.uast.uast-kotlin' module
    ```
    
    After this commit:
    ```
    There are circular dependencies between:
    1. Tests of 'kotlin.j2k.old' module, tests of 'kotlin.gradle.gradle-tooling' module,
       tests of 'kotlin.test-framework' module
    2. Tests of 'kotlin.maven' module, tests of 'kotlin.idea.tests' module
    ```
    
    (cherry picked from commit 629f0f5ddeedc2542262aede2de500cb7fdb3c52)
    
    GitOrigin-RevId: 2a3017a2cf2e67f3c3eeb5bca78594e2abf35ac3

--- generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1316,7 +1316,7 @@ private fun assembleWorkspace(): TWorkspace = workspace {
         }
     }
 
-    testGroup("j2k/new") {
+    testGroup("j2k/new/tests") {
         testClass<AbstractNewJavaToKotlinConverterSingleFileTest> {
             model("newJ2k", pattern = """^([^\.]+)\.java$""".toRegex())
         }
