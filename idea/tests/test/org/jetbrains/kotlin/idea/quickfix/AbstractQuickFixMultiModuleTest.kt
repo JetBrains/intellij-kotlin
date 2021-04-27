fcfa0589488defc8e0d45f4b8bbf340dff892a15
Leonid Startsev <leonid.startsev@jetbrains.com>
2021-04-28 01:48:11 +0300

    Respect explicit api mode state when generating actual declarations
    #KT-44067 Fixed
    
    Original commit: 796d2fd210c3c1a945609645757f612c7889ff40
    
    GitOrigin-RevId: f8a1e577dc69b420a940ad73cf452f0f628b6762

--- idea/tests/test/org/jetbrains/kotlin/idea/quickfix/AbstractQuickFixMultiModuleTest.kt
+++ idea/tests/test/org/jetbrains/kotlin/idea/quickfix/AbstractQuickFixMultiModuleTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -15,9 +15,7 @@ import junit.framework.TestCase
 import org.jetbrains.kotlin.idea.inspections.findExistingEditor
 import org.jetbrains.kotlin.idea.multiplatform.setupMppProjectFromDirStructure
 import org.jetbrains.kotlin.idea.stubs.AbstractMultiModuleTest
-import org.jetbrains.kotlin.idea.test.DirectiveBasedActionUtils
-import org.jetbrains.kotlin.idea.test.allKotlinFiles
-import org.jetbrains.kotlin.idea.test.findFileWithCaret
+import org.jetbrains.kotlin.idea.test.*
 import org.jetbrains.kotlin.idea.util.application.executeCommand
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.idea.test.InTextDirectivesUtils
@@ -40,7 +38,10 @@ abstract class AbstractQuickFixMultiModuleTest : AbstractMultiModuleTest(), Quic
 
     fun doTest(unused: String) {
         setupMppProjectFromDirStructure(testDataFile())
-        doQuickFixTest(fileName())
+        val directiveFileText = project.findFileWithCaret().text
+        withCustomCompilerOptions(directiveFileText, project, module) {
+            doQuickFixTest(fileName())
+        }
     }
 
     private fun doQuickFixTest(dirPath: String) {
