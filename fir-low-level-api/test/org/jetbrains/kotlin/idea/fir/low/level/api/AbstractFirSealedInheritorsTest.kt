02515bd15283d9a61be940076d7f57081f79f88f
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2021-04-29 17:44:07 +0300

    [FIR] Fix all usages of declarationSiteSession
    Original commit: 5cfa8694d4ed7331c5ad901d80ecfd39c91d45b7
    
    GitOrigin-RevId: 2856c1c167bb1621a90f48ba479d4f95c8962024

--- fir-low-level-api/test/org/jetbrains/kotlin/idea/fir/low/level/api/AbstractFirSealedInheritorsTest.kt
+++ fir-low-level-api/test/org/jetbrains/kotlin/idea/fir/low/level/api/AbstractFirSealedInheritorsTest.kt
@@ -22,7 +22,7 @@ abstract class AbstractFirSealedInheritorsTest : AbstractFirMultiModuleLazyResol
         val allClasses = firFile.listNestedClasses().closure { it.listNestedClasses() }
         val inheritorNames = allClasses.flatMap { firClass ->
             if (firClass.isSealed) {
-                firClass.getSealedClassInheritors(firFile.declarationSiteSession)
+                firClass.getSealedClassInheritors(firFile.moduleData.session)
             } else {
                 emptyList()
             }
