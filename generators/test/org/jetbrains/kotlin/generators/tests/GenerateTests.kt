a8740a9e6a93d2a8aabca69d99ed2d809402d013
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2021-05-10 17:09:02 +0200

    [FIR IDE] Move and rename RawFirFragmentForLazyBodiesBuilder into Fir Ide module
    Original commit: c5372be2671b731190511310a5a726ffb9bc315b
    
    GitOrigin-RevId: e8847988958f79b055d46828891df678e9e559d6

--- generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1054,6 +1054,9 @@ private fun assembleWorkspace(): TWorkspace = workspace {
         testClass<AbstractInnerDeclarationsResolvePhaseTest> {
             model("innerDeclarationsResolve")
         }
+        testClass<AbstractPartialRawFirBuilderTestCase> {
+            model("partialRawBuilder", testMethod = "doRawFirTest")
+        }
     }
 
 
