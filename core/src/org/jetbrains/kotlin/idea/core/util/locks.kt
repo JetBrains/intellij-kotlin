9772b7af554f64da27a18dad1d8908e56bb975a4
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2021-05-14 16:52:01 +0200

    Lock clean up: use reentrant lock instead of read-write reentrant lock
    Relates to #KT-46215
    
    GitOrigin-RevId: 685b7c148512e9bb4a7fc494840804ac33072ad2

--- core/src/org/jetbrains/kotlin/idea/core/util/locks.kt
+++ core/src/org/jetbrains/kotlin/idea/core/util/locks.kt
@@ -7,25 +7,6 @@ package org.jetbrains.kotlin.idea.core.util
 import com.intellij.openapi.progress.ProgressManager
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.locks.Lock
-import java.util.concurrent.locks.ReentrantReadWriteLock
-
-internal inline fun <T> ReentrantReadWriteLock.writeWithCheckCanceled(action: () -> T): T {
-    val rl = readLock()
-
-    val readCount = if (writeHoldCount == 0) readHoldCount else 0
-    repeat(readCount) { rl.unlock() }
-
-    val wl = writeLock()
-    while (!wl.tryLock(100, TimeUnit.MILLISECONDS)) {
-        ProgressManager.checkCanceled()
-    }
-    try {
-        return action()
-    } finally {
-        repeat(readCount) { rl.lock() }
-        wl.unlock()
-    }
-}
 
 internal inline fun <T> Lock.withCheckCanceledLock(action: () -> T): T {
     while (!tryLock(100, TimeUnit.MILLISECONDS)) {
