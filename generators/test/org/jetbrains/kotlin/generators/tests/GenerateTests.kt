c1c3347497580a74cf93c4eaa72e14d6ad643638
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-05-20 16:39:20 +0200

    FIR IDE: restore test generator after rebase
    Original commit: acda71cc90f983155e53140395821e96ef642312
    
    GitOrigin-RevId: 288a2d395bd6452898f1f6689e656de2ebdabbb3

--- generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1118,6 +1118,9 @@ private fun assembleWorkspace(): TWorkspace = workspace {
         testClass<AbstractHighLevelQuickFixTest> {
             val pattern = Patterns.forRegex("^([\\w\\-_]+)\\.kt$")
             model("quickfix/abstract", pattern = pattern)
+            model("quickfix/addExclExclCall", pattern = pattern, filenameStartsLowerCase = true)
+            model("quickfix/addInitializer", pattern = pattern, filenameStartsLowerCase = true)
+            model("quickfix/addPropertyAccessors", pattern = pattern, filenameStartsLowerCase = true)
             model("quickfix/expressions", pattern = pattern)
             model("quickfix/lateinit", pattern = pattern)
             model("quickfix/modifiers", pattern = pattern, isRecursive = false)
@@ -1125,9 +1128,8 @@ private fun assembleWorkspace(): TWorkspace = workspace {
             model("quickfix/replaceWithDotCall", pattern = pattern)
             model("quickfix/replaceWithSafeCall", pattern = pattern)
             model("quickfix/variables/changeMutability", pattern = pattern, isRecursive = false)
-            model("quickfix/addInitializer", pattern = pattern)
-            model("quickfix/addPropertyAccessors", pattern = pattern)
             model("quickfix/when", pattern = pattern, filenameStartsLowerCase = true)
+            model("quickfix/wrapWithSafeLetCall", pattern = pattern, filenameStartsLowerCase = true)
         }
 
         testClass<AbstractHighLevelQuickFixMultiFileTest> {
