c3c60af444e901259f67412f3c4a246c0ee5da7b
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-05-21 20:26:53 +0200

    Fix compilation after cherry-pick from Kotlin repo
    GitOrigin-RevId: 6d3d28f09a9efee006435a6a08e4f5a2741cc905

--- analysis/src/org/jetbrains/kotlin/idea/decompiler/navigation/SourceNavigationHelper.kt
+++ analysis/src/org/jetbrains/kotlin/idea/decompiler/navigation/SourceNavigationHelper.kt
@@ -38,7 +38,7 @@ import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.debugText.getDebugText
 import org.jetbrains.kotlin.platform.isCommon
-import org.jetbrains.kotlin.utils.addToStdlib.firstNotNullResult
+import org.jetbrains.kotlin.util.firstNotNullResult
 
 object SourceNavigationHelper {
     private val LOG = Logger.getInstance(SourceNavigationHelper::class.java)
