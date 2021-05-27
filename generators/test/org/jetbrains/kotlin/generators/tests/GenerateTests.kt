72bfe6c52a3d9a7e190e6a6ddcd12ec0556e12a1
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-05-27 15:06:30 +0200

    FIR IDE: add tests for completion in sealed when
    Original commit: 0da488058df2e726368c6b4827aae52dfeb0930a
    
    GitOrigin-RevId: 8570bf99eeb2b995d8ed61c54713783cfb9d01d5

--- generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1190,6 +1190,7 @@ private fun assembleWorkspace(): TWorkspace = workspace {
         testClass<AbstractHighLevelJvmBasicCompletionTest> {
             model("basic/common")
             model("basic/java")
+            model("../../idea-fir/testData/completion/basic/common", testClassName = "CommonFir")
         }
 
         testClass<AbstractHighLevelBasicCompletionHandlerTest> {
