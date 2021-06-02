02f3106ee8dfd20abb528bf3ecfb6d29b96dc21b
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-06-02 14:08:51 +0200

    [kotlin] Download compiler testdata for tests which requires it
    GitOrigin-RevId: a00cb617bb9bf7f068155bec3ca3d45ca893f248

--- generators/test/org/jetbrains/kotlin/testGenerator/model/TGroup.kt
+++ generators/test/org/jetbrains/kotlin/testGenerator/model/TGroup.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.kotlin.testGenerator.model
 
+import org.jetbrains.kotlin.idea.artifacts.AdditionalKotlinArtifacts
 import org.jetbrains.kotlin.test.KotlinRoot
 import java.io.File
 
@@ -15,17 +16,24 @@ interface TGroup {
     val testDataRoot: File
 
     val suites: List<TSuite>
+
+    val isCompilerTestData: Boolean
 }
 
 interface MutableTGroup : TGroup {
     override val suites: MutableList<TSuite>
 }
 
-class TGroupImpl(override val modulePath: String, override val testSourcesPath: String, override val testDataPath: String) : MutableTGroup {
+class TGroupImpl(
+    override val modulePath: String,
+    override val testSourcesPath: String,
+    override val testDataPath: String,
+    override val isCompilerTestData: Boolean,
+) : MutableTGroup {
     override val kotlinRoot = KotlinRoot.DIR
     override val moduleRoot = File(kotlinRoot, modulePath)
     override val testSourcesRoot = File(moduleRoot, testSourcesPath)
-    override val testDataRoot = File(moduleRoot, testDataPath)
+    override val testDataRoot = if (testDataPath.startsWith("/")) File(testDataPath) else File(moduleRoot, testDataPath)
     override val suites = mutableListOf<TSuite>()
 }
 
@@ -35,5 +43,10 @@ fun MutableTWorkspace.testGroup(
     testDataPath: String = "testData",
     block: MutableTGroup.() -> Unit
 ) {
-    groups += TGroupImpl(modulePath, testSourcesPath, testDataPath).apply(block)
+    groups += TGroupImpl(
+        modulePath,
+        testSourcesPath,
+        testDataPath,
+        isCompilerTestData = testDataPath.startsWith(AdditionalKotlinArtifacts.compilerTestDataDir.canonicalPath)
+    ).apply(block)
 }
