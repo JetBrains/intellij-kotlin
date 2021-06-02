aaff6ecc03fb7f8c6a5fd11aec11768b48cf0b8d
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-06-02 17:01:58 +0200

    Wizard: update testdata after updating compose/android gradle plugin versions
    GitOrigin-RevId: a2302778cfc83613567ae8ac17edf1cbe2d46256

--- project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeMultiplatformApplication/expected/build.gradle.kts
+++ project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeMultiplatformApplication/expected/build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
-        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("com.android.tools.build:gradle:4.0.2")
     }
 }
 
