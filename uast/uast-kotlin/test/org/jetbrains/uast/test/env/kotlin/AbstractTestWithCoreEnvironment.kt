cee439a6dcd9dec35b4d70d839b7d39e98fe8e76
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-06-04 19:50:17 +0200

    Fix compilation after cherry-pick from Kotlin repo
    GitOrigin-RevId: 0772313a65b7f579b55d018a4e064b4e8c9ab77c

--- uast/uast-kotlin/test/org/jetbrains/uast/test/env/kotlin/AbstractTestWithCoreEnvironment.kt
+++ uast/uast-kotlin/test/org/jetbrains/uast/test/env/kotlin/AbstractTestWithCoreEnvironment.kt
@@ -75,23 +75,3 @@ abstract class AbstractTestWithCoreEnvironment : TestCase() {
         myEnvironment = null
     }
 }
-
-private fun String.trimTrailingWhitespacesAndAddNewlineAtEOF(): String =
-        this.split('\n').map(String::trimEnd).joinToString(separator = "\n").let {
-            result -> if (result.endsWith("\n")) result else result + "\n"
-        }
-
-fun assertEqualsToFile(description: String, expected: File, actual: String) {
-    if (!expected.exists()) {
-        expected.writeText(actual)
-        TestCase.fail("File didn't exist. New file was created (${expected.canonicalPath}).")
-    }
-
-    val expectedText =
-            StringUtil.convertLineSeparators(expected.readText().trim()).trimTrailingWhitespacesAndAddNewlineAtEOF()
-    val actualText =
-            StringUtil.convertLineSeparators(actual.trim()).trimTrailingWhitespacesAndAddNewlineAtEOF()
-    if (expectedText != actualText) {
-        throw FileComparisonFailure(description, expectedText, actualText, expected.absolutePath)
-    }
-}
