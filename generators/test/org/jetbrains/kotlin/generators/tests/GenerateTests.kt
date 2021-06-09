bc431fd0bde0dbecf6579cf58e3cf1518162479d
Mark Punzalan <markpun@google.com>
2021-06-09 08:36:54 +0000

    FIR IDE: Enable add accessor intentions in plugin.
    Original commit: a497dd1d31d7643fde7248b3d32bbb312015ef2a
    
    GitOrigin-RevId: fcfd9907a9b68fa6a2b26fcc6592e74f1fcbd23a

--- generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1165,6 +1165,7 @@ private fun assembleWorkspace(): TWorkspace = workspace {
 
         testClass<AbstractHLIntentionTest> {
             val pattern = Patterns.forRegex("^([\\w\\-_]+)\\.(kt|kts)$")
+            model("intentions/addPropertyAccessors", pattern = pattern)
             model("intentions/specifyTypeExplicitly", pattern = pattern)
         }
 
