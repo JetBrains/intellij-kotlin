082c6ceba310064e2d1afe1a6bbd0b5b4ef4406d
Vladimir Dolzhenko <Vladimir.Dolzhenko@jetbrains.com>
2021-06-09 08:31:46 +0000

    Optimize SdkInfo lookup for a huge monorepo with no SdkInfo
    DFS of SdkInfo on moduleInfo and reuse cached libraryInfo dependencies
    fix traverse order
    
    #KT-47065 Fixed
    Relates to #KT-46622
    Original commit: de76aeb9d1de6d927c7a2a5041dc2ad8105e214b
    
    GitOrigin-RevId: bbff12f01f8937f9fda66d14008cf45933159364

--- analysis/src/org/jetbrains/kotlin/idea/caches/project/LibraryDependenciesCache.kt
+++ analysis/src/org/jetbrains/kotlin/idea/caches/project/LibraryDependenciesCache.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.idea.caches.project
 import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.*
 import com.intellij.openapi.roots.impl.libraries.LibraryEx
@@ -107,6 +108,7 @@ class LibraryDependenciesCacheImpl(private val project: Project) : LibraryDepend
         val platform = libraryInfo.platform
 
         for (module in getLibraryUsageIndex().getModulesLibraryIsUsedIn(libraryInfo)) {
+            ProgressManager.checkCanceled()
             val (moduleLibraries, moduleSdks) = moduleDependenciesCache.getOrPut(module) {
                 computeLibrariesAndSdksUsedIn(module)
             }
