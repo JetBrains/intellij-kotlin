6f7ae37015e2fea51c1256c26dcb9a105f02efb1
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-06-28 20:26:13 +0700

    KtArrayAccessReference: remove redundant functions
    GitOrigin-RevId: c0596d241846b40b99701022e3c10c76abcec0e5

--- analysis/src/org/jetbrains/kotlin/idea/references/KtArrayAccessReferenceDescriptorsImpl.kt
+++ analysis/src/org/jetbrains/kotlin/idea/references/KtArrayAccessReferenceDescriptorsImpl.kt
@@ -8,8 +8,6 @@ package org.jetbrains.kotlin.idea.references
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.psi.KtArrayAccessExpression
-import org.jetbrains.kotlin.psi.KtCallExpression
-import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.BindingContext.INDEXED_LVALUE_GET
 import org.jetbrains.kotlin.resolve.BindingContext.INDEXED_LVALUE_SET
@@ -19,17 +17,7 @@ internal class KtArrayAccessReferenceDescriptorsImpl(
 ) : KtArrayAccessReference(expression), KtDescriptorsBasedReference {
     override fun isReferenceTo(element: PsiElement): Boolean = super<KtDescriptorsBasedReference>.isReferenceTo(element)
 
-    override fun moveFunctionLiteralOutsideParentheses(callExpression: KtCallExpression) {
-        TODO("compiler should be fixed and this override isn't needed")
-    }
-
-    override fun canMoveLambdaOutsideParentheses(callExpression: KtCallExpression): Boolean {
-        TODO("compiler should be fixed and this override isn't needed")
-    }
-
-    override fun handleElementRename(newElementName: String): PsiElement = doRenameImplicitConventionalCall(newElementName)
-
-    override fun doRenameImplicitConventionalCall(newName: String?): KtExpression = renameImplicitConventionalCall(newName)
+    override fun handleElementRename(newElementName: String): PsiElement = renameImplicitConventionalCall(newElementName)
 
     override fun getTargetDescriptors(context: BindingContext): Collection<DeclarationDescriptor> {
         val getFunctionDescriptor = context[INDEXED_LVALUE_GET, expression]?.candidateDescriptor
