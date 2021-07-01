b617334ed540198ca559dd6fc32d97d4e2c01ec4
Jinseong Jeon <jsjeon@google.com>
2021-06-30 22:07:50 -0700

    FIR/UAST: commonize declaration expression
    GitOrigin-RevId: 5208e54094e3aa83dd7125370a11281e1d6376f3

--- uast/uast-kotlin/src/org/jetbrains/uast/kotlin/KotlinConverter.kt
+++ uast/uast-kotlin/src/org/jetbrains/uast/kotlin/KotlinConverter.kt
@@ -77,8 +77,7 @@ internal object KotlinConverter : BaseKotlinConverter {
 
         return with (requiredTypes) { when (element) {
             is KtParameterList -> el<UDeclarationsExpression> {
-                val resolveProviderService = ServiceManager.getService(project, KotlinUastResolveProviderService::class.java)
-                val declarationsExpression = KotlinUDeclarationsExpression(null, givenParent, service, null) { resolveProviderService }
+                val declarationsExpression = KotlinUDeclarationsExpression(null, givenParent, service, null) { service }
                 declarationsExpression.apply {
                     declarations = element.parameters.mapIndexed { i, p ->
                         KotlinUParameter(UastKotlinPsiParameter.create(p, element, declarationsExpression, i), p, this)
@@ -93,7 +92,7 @@ internal object KotlinConverter : BaseKotlinConverter {
                 }
                 else {
                     el<UVariable> { convertVariablesDeclaration(element, givenParent).declarations.singleOrNull() }
-                        ?: expr<UDeclarationsExpression> { KotlinConverter.convertExpression(element, givenParent, requiredTypes) }
+                        ?: expr<UDeclarationsExpression> { convertExpression(element, givenParent, requiredTypes) }
                 }
 
             is KtExpression -> convertExpression(element, givenParent, requiredTypes)
