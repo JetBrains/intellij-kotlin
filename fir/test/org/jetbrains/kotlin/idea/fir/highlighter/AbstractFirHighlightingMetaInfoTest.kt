7ade28b12908ec9908f28c28bae6c11004be1014
Roman Golyshev <roman.golyshev@jetbrains.com>
2021-07-07 19:25:29 +0300

    Add highlighting tests based on using `CodeMetaInfo` in testData
    Those tests should be more robust than the old ones, since
    we can directly control how we render highlighting data, and
    do not need to rely on platform's (rather complicated)
    `com.intellij.testFramework.fixtures.CodeInsightTestFixture.checkHighlighting`
    
    Some test might still contain some ERROR severity information
    
    GitOrigin-RevId: 685bc90d7e41cfbca7315c017c5db890e4d10a48

---
+++ fir/test/org/jetbrains/kotlin/idea/fir/highlighter/AbstractFirHighlightingMetaInfoTest.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.idea.fir.highlighter
+
+import org.jetbrains.kotlin.idea.highlighter.AbstractHighlightingMetaInfoTest
+import org.jetbrains.kotlin.idea.test.ProjectDescriptorWithStdlibSources
+import org.jetbrains.kotlin.test.utils.IgnoreTests
+
+abstract class AbstractFirHighlightingMetaInfoTest : AbstractHighlightingMetaInfoTest() {
+    override fun isFirPlugin(): Boolean = true
+
+    override fun getDefaultProjectDescriptor() = ProjectDescriptorWithStdlibSources.INSTANCE
+
+    override fun doTest(unused: String) {
+      IgnoreTests.runTestIfNotDisabledByFileDirective(
+          testDataFile().toPath(),
+          disableTestDirective = IgnoreTests.DIRECTIVES.IGNORE_FIR,
+          additionalFilesExtensions = arrayOf(HIGHLIGHTING_EXTENSION)
+      ) {
+        // warnings are not supported yet
+        super.doTest(unused)
+      }
+    }
+}
--- generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -105,6 +105,7 @@ import org.jetbrains.kotlin.idea.fir.frontend.api.components.AbstractExpectedExp
 import org.jetbrains.kotlin.idea.fir.frontend.api.components.AbstractHLExpressionTypeTest
 import org.jetbrains.kotlin.idea.fir.frontend.api.components.AbstractOverriddenDeclarationProviderTest
 import org.jetbrains.kotlin.idea.fir.frontend.api.components.AbstractRendererTest
+import org.jetbrains.kotlin.idea.fir.highlighter.AbstractFirHighlightingMetaInfoTest
 import org.jetbrains.kotlin.idea.fir.inspections.AbstractFe10BindingIntentionTest
 import org.jetbrains.kotlin.idea.fir.low.level.api.sessions.AbstractSessionsInvalidationTest
 import org.jetbrains.kotlin.idea.fir.low.level.api.trackers.AbstractProjectWideOutOfBlockKotlinModificationTrackerTest
@@ -448,6 +449,10 @@ private fun assembleWorkspace(): TWorkspace = workspace {
 
         testClass<AbstractHighlightingTest> {
             model("highlighter")
+        }
+
+        testClass<AbstractHighlightingMetaInfoTest> {
+            model("highlighterMetaInfo")
         }
 
         testClass<AbstractDslHighlighterTest> {
@@ -1035,6 +1040,10 @@ private fun assembleWorkspace(): TWorkspace = workspace {
         testClass<AbstractFirHighlightingTest> {
             model("highlighter")
             model("../../../fir/testData/highlighterFir", pattern = KT_WITHOUT_DOTS)
+        }
+
+        testClass<AbstractFirHighlightingMetaInfoTest> {
+            model("highlighterMetaInfo")
         }
 
         testClass<AbstractFirKotlinHighlightingPassTest> {
