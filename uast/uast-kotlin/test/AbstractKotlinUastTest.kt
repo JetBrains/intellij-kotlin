50ae8f2f709be55d404f7b9cf9c8ff247c95d491
Jinseong Jeon <jsjeon@google.com>
2021-07-09 22:54:12 -0700

    FIR/UAST: make base service application-wide available

--- uast/uast-kotlin/test/AbstractKotlinUastTest.kt
+++ uast/uast-kotlin/test/AbstractKotlinUastTest.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.uast.test.kotlin
 
+import com.intellij.mock.MockComponentManager
 import com.intellij.mock.MockProject
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
@@ -7,6 +8,8 @@ import com.intellij.openapi.extensions.Extensions
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.testFramework.registerComponentImplementation
+import com.intellij.testFramework.replaceService
 import com.intellij.util.io.URLUtil
 import org.jetbrains.kotlin.idea.checkers.CompilerTestLanguageVersionSettings
 import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
@@ -95,7 +98,8 @@ abstract class AbstractKotlinUastTest : AbstractUastTest() {
         area.getExtensionPoint(UastLanguagePlugin.extensionPointName).registerExtension(KotlinUastLanguagePlugin(), project)
         area.getExtensionPoint(UEvaluatorExtension.EXTENSION_POINT_NAME).registerExtension(KotlinEvaluatorExtension(), project)
 
-        project.registerService(
+        val application = ApplicationManager.getApplication() as MockComponentManager
+        application.registerService(
             BaseKotlinUastResolveProviderService::class.java,
             CliKotlinUastResolveProviderService::class.java
         )
