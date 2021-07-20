c9eb520ee044175299358fa44fef62ecfd82e3c3
Jinseong Jeon <jsjeon@google.com>
2021-07-20 00:27:21 -0700

    FIR/UAST: resolved function name from KtCallElement
    GitOrigin-RevId: 0a9bdbf1460bae6fe883fb746278acef36cbad25

--- uast/uast-kotlin/src/org/jetbrains/uast/kotlin/expressions/KotlinUFunctionCallExpression.kt
+++ uast/uast-kotlin/src/org/jetbrains/uast/kotlin/expressions/KotlinUFunctionCallExpression.kt
@@ -38,7 +38,9 @@ class KotlinUFunctionCallExpression(
         baseResolveProviderService.getReceiverType(sourcePsi, this)
     }
 
-    override val methodName by lz { resolvedCall?.resultingDescriptor?.name?.asString() }
+    override val methodName by lz {
+        baseResolveProviderService.resolvedFunctionName(sourcePsi)
+    }
 
     override val classReference by lz {
         KotlinClassViaConstructorUSimpleReferenceExpression(sourcePsi, methodName.orAnonymous("class"), this)
