650f3c80f357fca1320979b4205544d0ec3e002c
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2021-07-26 19:51:11 +0300

    Fix compilation: add stubs for FIR UAST service
    GitOrigin-RevId: fa489688226206b357273b24575999c01a11b516

--- uast/uast-kotlin-idea-fir/src/org/jetbrains/uast/kotlin/internal/FirIdeaKotlinUastResolveProviderService.kt
+++ uast/uast-kotlin-idea-fir/src/org/jetbrains/uast/kotlin/internal/FirIdeaKotlinUastResolveProviderService.kt
@@ -6,13 +6,45 @@
 package org.jetbrains.uast.kotlin.internal
 
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiType
+import org.jetbrains.kotlin.psi.KtDoubleColonExpression
 import org.jetbrains.kotlin.psi.KtExpression
-import org.jetbrains.uast.kotlin.FirKotlinUastResolveProviderService
+import org.jetbrains.kotlin.psi.KtTypeReference
+import org.jetbrains.uast.UElement
+import org.jetbrains.uast.UExpression
+import org.jetbrains.uast.kotlin.BaseKotlinConverter
+import org.jetbrains.uast.kotlin.BaseKotlinUastResolveProviderService
 
-class FirIdeaKotlinUastResolveProviderService : FirKotlinUastResolveProviderService {
+class FirIdeaKotlinUastResolveProviderService : BaseKotlinUastResolveProviderService {
     override fun isJvmElement(psiElement: PsiElement): Boolean = psiElement.isJvmElement
+    override val baseKotlinConverter: BaseKotlinConverter
+        get() = TODO("Not yet implemented")
+
+    override fun convertParent(uElement: UElement): UElement? {
+        TODO("Not yet implemented")
+    }
 
     override fun getReferenceVariants(ktExpression: KtExpression, nameHint: String): Sequence<PsiElement> {
+        TODO("Not yet implemented")
+    }
+
+    override fun resolveToDeclaration(ktExpression: KtExpression): PsiElement? {
+        TODO("Not yet implemented")
+    }
+
+    override fun resolveToType(ktTypeReference: KtTypeReference, source: UElement): PsiType? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getDoubleColonReceiverType(ktDoubleColonExpression: KtDoubleColonExpression, source: UElement): PsiType? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getExpressionType(uExpression: UExpression): PsiType? {
+        TODO("Not yet implemented")
+    }
+
+    override fun evaluate(uExpression: UExpression): Any? {
         TODO("Not yet implemented")
     }
 }
