029a7df0b7f4fcac84b7b04e29d9fc8ece0f3435
Igor Demin <Igor.Demin@jetbrains.com>
2021-08-09 09:29:54 +0000

    Update compose version to 1.0.0-alpha3
    Fixes https://youtrack.jetbrains.com/issue/KTIJ-19331
    
    Co-authored-by: Igor Demin <igordmn@gmail.com>
    
    # Conflicts:
    #	kotlin/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeMultiplatformApplication/expected/android/build.gradle.kts
    
    GitOrigin-RevId: 4a892287e76d4428c144c8b8fe6c6ceebc9d57cc

--- project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/templates/compose/ComposeAndroidTemplate.kt
+++ project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/templates/compose/ComposeAndroidTemplate.kt
@@ -52,7 +52,6 @@ class ComposeAndroidTemplate : Template() {
     ) = irsList {
         +GradleOnlyPluginByNameIR("org.jetbrains.compose", version = Versions.JETBRAINS_COMPOSE)
         +RepositoryIR(Repositories.JETBRAINS_COMPOSE_DEV)
-        +RepositoryIR(DefaultRepository.JCENTER)
         +RepositoryIR(DefaultRepository.GOOGLE)
         +Dependencies.ACTIVITY_COMPOSE
     }
@@ -76,7 +75,7 @@ class ComposeAndroidTemplate : Template() {
     }
 
     override fun Writer.runArbitratyTask(module: ModuleIR): TaskResult<Unit> = compute {
-        BuildSystemPlugin.pluginRepositoreis.addValues(Repositories.JETBRAINS_COMPOSE_DEV).ensure()
+        BuildSystemPlugin.pluginRepositoreis.addValues(Repositories.JETBRAINS_COMPOSE_DEV, DefaultRepository.GOOGLE).ensure()
 
         //TODO hacky!
         TemplatesPlugin.fileTemplatesToRender.update { templates ->
