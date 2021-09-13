ed8b650611d298887b937003455f74cc030dcbda
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2021-09-13 13:21:48 +0300

    [kotlin] Add test on KT-48291
    The related changes are on the compiler side. See KT-48291 for details
    
    Cherry-picked from b61da6bcde08d3f9f6bed2c81bdd832aca6d3a1e
    
    GitOrigin-RevId: 194d70fc86da37fb5a06cc94437dd493929f0acd

---
+++ idea/tests/testData/multiplatform/kt48291/jvm/I_JVM.kt
@@ -0,0 +1,11 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package sample
+
+actual interface <!LINE_MARKER("descr='Has declaration in common module'"), LINE_MARKER("descr='Is implemented by CommonMainImpl [jvm]  Click or press ... to navigate'")!>CommonMainInterface<!> {
+    actual val <!LINE_MARKER("descr='Has declaration in common module'"), LINE_MARKER("descr='Is implemented in sample.CommonMainImpl'")!>propertyFromInterface<!>: Int
+}
+
+actual class <!LINE_MARKER("descr='Has declaration in common module'")!>CommonMainImpl<!> : CommonMainInterface {
+    override val <!LINE_MARKER("descr='Implements property in 'CommonMainInterface''")!>propertyFromInterface<!>: Int = 42
+    actual val <!LINE_MARKER("descr='Has declaration in common module'")!>propertyFromImpl<!>: Int = 42
+}
