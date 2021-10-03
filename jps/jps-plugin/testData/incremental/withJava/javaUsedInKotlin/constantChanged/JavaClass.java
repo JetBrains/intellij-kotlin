a539ab8cf19e66bd02aa9de8c7b4d9a372dc3942
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2021-10-03 03:27:53 +0200

    [testdata] Fix testdata for IncrementalJvmJpsTestGenerated.WithJava.JavaUsedInKotlin#testConstantChanged
    Previously, because the package in java file was used, generated test runner file was incorrect
    
    (cherry picked from commit 3fcf599a26a0789154c550e6ac4aafe0829ce0d1)
    
    GitOrigin-RevId: 67233dfc7306f768c20b8d9c453cc37571d6e164

--- jps/jps-plugin/testData/incremental/withJava/javaUsedInKotlin/constantChanged/JavaClass.java
+++ jps/jps-plugin/testData/incremental/withJava/javaUsedInKotlin/constantChanged/JavaClass.java
@@ -1,6 +1,4 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains;
-
 public class JavaClass {
     public class Inner {
         public static final String CONST = "A";
