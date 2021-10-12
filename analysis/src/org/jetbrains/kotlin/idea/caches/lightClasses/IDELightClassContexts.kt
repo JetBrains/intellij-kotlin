1d38f8ce7c5e6bc83188ff3a3900cb160844c3fc
Aleksei.Cherepanov <aleksei.cherepanov@jetbrains.com>
2021-10-12 18:00:36 +0200

    Fix incremental build after changing Java const used as class property
    Report Java static final constant with InlineConstantTracker, used as class property in Kotlin for further registration in JPS. Add tests.
    
    #KT-49177 Fixed
    
    GitOrigin-RevId: 7f98d72d0184b5c5fae339d3621b987c4771cea8

--- analysis/src/org/jetbrains/kotlin/idea/caches/lightClasses/IDELightClassContexts.kt
+++ analysis/src/org/jetbrains/kotlin/idea/caches/lightClasses/IDELightClassContexts.kt
@@ -37,6 +37,7 @@ import org.jetbrains.kotlin.idea.project.languageVersionSettings
 import org.jetbrains.kotlin.idea.resolve.ResolutionFacade
 import org.jetbrains.kotlin.idea.stubindex.KotlinOverridableInternalMembersShortNameIndex
 import org.jetbrains.kotlin.incremental.components.ExpectActualTracker
+import org.jetbrains.kotlin.incremental.components.InlineConstTracker
 import org.jetbrains.kotlin.incremental.components.LookupTracker
 import org.jetbrains.kotlin.incremental.components.NoLookupLocation
 import org.jetbrains.kotlin.lexer.KtTokens
@@ -382,6 +383,7 @@ internal object IDELightClassContexts {
             useInstance(GlobalSearchScope.EMPTY_SCOPE)
             useInstance(LookupTracker.DO_NOTHING)
             useInstance(ExpectActualTracker.DoNothing)
+            useInstance(InlineConstTracker.DoNothing)
             useImpl<FileScopeProviderImpl>()
             useInstance(FileBasedDeclarationProviderFactory(sm, files))
 
