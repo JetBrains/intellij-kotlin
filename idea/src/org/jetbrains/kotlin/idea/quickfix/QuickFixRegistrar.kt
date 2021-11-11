JetBrains/intellij-community

[kotlin] Updated opt-in messages, fixed a bug with @file annotations
^KTIJ-11328 Fixed
^KTIJ-18865 Fixed
^KTIJ-18439 Fixed
^KTIJ-18291 Fixed

GitOrigin-RevId: 3bf11786a4903d6747ffdaf568f114129a9e81fd

plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
@@ -597,6 +597,7 @@ class QuickFixRegistrar : QuickFixContributor {
         EXPERIMENTAL_ANNOTATION_WITH_WRONG_TARGET.registerFactory(RemoveWrongOptInAnnotationTargetFactory)
         EXPERIMENTAL_ANNOTATION_ON_OVERRIDE.registerFactory(RemoveAnnotationFix)
         EXPERIMENTAL_ANNOTATION_ON_OVERRIDE_WARNING.registerFactory(RemoveAnnotationFix)
+        USE_EXPERIMENTAL_WITHOUT_ARGUMENTS.registerFactory(RemoveAnnotationFix)
 
         TYPE_VARIANCE_CONFLICT.registerFactory(RemoveTypeVarianceFix, AddAnnotationFix.TypeVarianceConflictFactory)
 

JetBrains/intellij-kotlin

(cherry picked from commit 3bf11786a4903d6747ffdaf568f114129a9e81fd)
GitOrigin-RevId: e9037a03308a2faa57af5ac6432ddc3e4762c9ed

idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
@@ -624,6 +624,7 @@ class QuickFixRegistrar : QuickFixContributor {
         OPT_IN_MARKER_WITH_WRONG_TARGET.registerFactory(RemoveWrongOptInAnnotationTargetFactory)
         OPT_IN_MARKER_ON_OVERRIDE.registerFactory(RemoveAnnotationFix)
         OPT_IN_MARKER_ON_OVERRIDE_WARNING.registerFactory(RemoveAnnotationFix)
+        OPT_IN_WITHOUT_ARGUMENTS.registerFactory(RemoveAnnotationFix)
 
 
         TYPE_VARIANCE_CONFLICT.registerFactory(RemoveTypeVarianceFix)