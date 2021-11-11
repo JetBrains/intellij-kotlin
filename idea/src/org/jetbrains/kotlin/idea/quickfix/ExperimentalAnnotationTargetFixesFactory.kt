JetBrains/intellij-community

[kotlin] Quick fixes for forbidden opt-in targets
Quick fixes for opt-in usage errors:
- remove forbidden annotation target values at declaration;
- remove annotation that cannot be used at the given use site;
- replace getter annotation with the property annotation;
- replace value parameter annotation with the property annotation.

^KTIJ-18865 In Progress

GitOrigin-RevId: b7bda312264d0e68f72d105359c5b16b7488a9eb

plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/ExperimentalAnnotationTargetFixesFactory.kt
@@ -1,0 +1,100 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.kotlin.idea.quickfix
+
+import com.intellij.codeInsight.intention.HighPriorityAction
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.psi.SmartPsiElementPointer
+import org.jetbrains.kotlin.descriptors.annotations.AnnotationUseSiteTarget
+import org.jetbrains.kotlin.diagnostics.Diagnostic
+import org.jetbrains.kotlin.diagnostics.Errors
+import org.jetbrains.kotlin.idea.KotlinBundle
+import org.jetbrains.kotlin.idea.caches.resolve.analyze
+import org.jetbrains.kotlin.idea.inspections.RemoveAnnotationFix
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi.KtAnnotationEntry
+import org.jetbrains.kotlin.psi.KtModifierListOwner
+import org.jetbrains.kotlin.psi.KtParameter
+import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.psi.psiUtil.createSmartPointer
+import org.jetbrains.kotlin.psi.psiUtil.getParentOfTypes
+import org.jetbrains.kotlin.renderer.render
+import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
+import org.jetbrains.kotlin.utils.addToStdlib.safeAs
+
+/**
+ * Quick fix factory for forbidden experimental annotation usage.
+ *
+ * Annotations that indicate the opt-in requirement are forbidden in several use sites:
+ * getters, value parameters, local variables. This factory generates quick fixes
+ * to either remove the annotation or to replace it with an allowed annotation variant
+ * (e.g., annotate a property instead of a getter or a value parameter).
+ */
+object ExperimentalAnnotationWrongTargetFixesFactory : KotlinIntentionActionsFactory() {
+    override fun doCreateActions(diagnostic: Diagnostic): List<IntentionAction> {
+        if (diagnostic.factory != Errors.EXPERIMENTAL_ANNOTATION_ON_WRONG_TARGET) return emptyList()
+        val annotationEntry = diagnostic.psiElement.safeAs<KtAnnotationEntry>() ?: return emptyList()
+        val annotationUseSiteTarget = annotationEntry.useSiteTarget?.getAnnotationUseSiteTarget()
+        val annotatedElement = annotationEntry.getParentOfTypes(
+            strict = true,
+            KtProperty::class.java,
+            KtParameter::class.java,
+        ) ?: return emptyList()
+
+        val result = mutableListOf<IntentionAction>()
+        val bindingContext = annotationEntry.analyze(BodyResolveMode.PARTIAL)
+        val annotationFqName = bindingContext[BindingContext.ANNOTATION, annotationEntry]?.fqName
+        if (annotationFqName != null) {
+            when {
+                annotatedElement is KtParameter && annotationUseSiteTarget != AnnotationUseSiteTarget.PROPERTY ->
+                    result.add(
+                        HighPriorityReplaceAnnotationFix(
+                            annotationEntry,
+                            annotatedElement.createSmartPointer(),
+                            annotationFqName,
+                            useSiteTarget = AnnotationUseSiteTarget.PROPERTY
+                        )
+                    )
+                annotatedElement is KtProperty ->
+                    result.add(
+                        HighPriorityMoveGetterAnnotationToPropertyFix(
+                            annotationEntry,
+                            annotatedElement.createSmartPointer(),
+                            annotationFqName
+                        )
+                    )
+            }
+        }
+
+        // 'Remove annotation' action is the universal fallback
+        result.addAll(RemoveAnnotationFix.createActions(diagnostic))
+        return result
+    }
+}
+
+/**
+ * High priority specialized version of [ReplaceAnnotationFix] with a custom description.
+ * It is used to move annotations from getters to corresponding properties.
+ *
+ * @param annotationEntry the annotation entry to move
+ * @param modifierListOwner the property whose getter is currently annotated
+ * @param annotationFqName fully qualified annotation class name
+ * @param existingReplacementAnnotationEntry the existing annotation to update (null by default)
+ */
+private class HighPriorityMoveGetterAnnotationToPropertyFix(
+    annotationEntry: KtAnnotationEntry,
+    modifierListOwner: SmartPsiElementPointer<KtModifierListOwner>,
+    annotationFqName: FqName,
+    existingReplacementAnnotationEntry: SmartPsiElementPointer<KtAnnotationEntry>? = null
+) : ReplaceAnnotationFix(
+    annotationEntry,
+    modifierListOwner,
+    annotationFqName,
+    argumentClassFqName = null,
+    useSiteTarget = null,
+    existingReplacementAnnotationEntry = existingReplacementAnnotationEntry
+), HighPriorityAction {
+    override fun getText(): String {
+        return KotlinBundle.message("fix.replace.annotation.move.from.getter.to.property", renderAnnotationText())
+    }
+}

plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
@@ -595,6 +595,10 @@ class QuickFixRegistrar : QuickFixContributor {
         EXPERIMENTAL_OVERRIDE.registerFactory(ExperimentalFixesFactory)
         EXPERIMENTAL_OVERRIDE_ERROR.registerFactory(ExperimentalFixesFactory)
         EXPERIMENTAL_IS_NOT_ENABLED.registerFactory(MakeModuleExperimentalFix)
+        EXPERIMENTAL_ANNOTATION_ON_WRONG_TARGET.registerFactory(ExperimentalAnnotationWrongTargetFixesFactory)
+        EXPERIMENTAL_ANNOTATION_WITH_WRONG_TARGET.registerFactory(RemoveWrongOptInAnnotationTargetFactory)
+        EXPERIMENTAL_ANNOTATION_ON_OVERRIDE.registerFactory(RemoveAnnotationFix)
+        EXPERIMENTAL_ANNOTATION_ON_OVERRIDE_WARNING.registerFactory(RemoveAnnotationFix)
 
         TYPE_VARIANCE_CONFLICT.registerFactory(RemoveTypeVarianceFix, AddAnnotationFix.TypeVarianceConflictFactory)
 

plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/RemoveWrongOptInAnnotationTargetFactory.kt
@@ -1,0 +1,49 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.kotlin.idea.quickfix
+
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import org.jetbrains.kotlin.diagnostics.Diagnostic
+import org.jetbrains.kotlin.diagnostics.Errors.EXPERIMENTAL_ANNOTATION_WITH_WRONG_TARGET
+import org.jetbrains.kotlin.idea.KotlinBundle
+import org.jetbrains.kotlin.psi.KtAnnotationEntry
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtValueArgument
+import org.jetbrains.kotlin.utils.addToStdlib.safeAs
+
+object RemoveWrongOptInAnnotationTargetFactory : KotlinIntentionActionsFactory() {
+    override fun doCreateActions(diagnostic: Diagnostic): List<IntentionAction> {
+        if (diagnostic.factory != EXPERIMENTAL_ANNOTATION_WITH_WRONG_TARGET) return emptyList()
+        val annotationEntry = diagnostic.psiElement.safeAs<KtAnnotationEntry>() ?: return emptyList()
+        return listOf(RemoveAllForbiddenOptInTargetsFix(annotationEntry))
+    }
+
+    private class RemoveAllForbiddenOptInTargetsFix(annotationEntry: KtAnnotationEntry) :
+        KotlinQuickFixAction<KtAnnotationEntry>(annotationEntry) {
+        private val forbiddenTargetValues: List<String> = listOf(
+            AnnotationTarget.EXPRESSION.toString(),
+            AnnotationTarget.FILE.toString(),
+            AnnotationTarget.TYPE.toString(),
+            AnnotationTarget.TYPE_PARAMETER.toString()
+        )
+
+        override fun getText(): String {
+            return KotlinBundle.message("fix.opt_in.remove.all.forbidden.targets")
+        }
+
+        override fun getFamilyName(): String = text
+
+        override fun invoke(project: Project, editor: Editor?, file: KtFile) {
+            val annotationEntry = element ?: return
+            val argumentList = annotationEntry.valueArgumentList ?: return
+            val forbiddenArguments: List<KtValueArgument> = argumentList.arguments.filter {
+                forbiddenTargetValues.any { name -> it.text?.contains(name) == true }
+            }
+
+            forbiddenArguments.forEach {
+                argumentList.removeArgument(it)
+            }
+        }
+    }
+}

JetBrains/intellij-kotlin

(cherry picked from commit b7bda312264d0e68f72d105359c5b16b7488a9eb)
GitOrigin-RevId: 69935fe431510d0694c335ca1701793f04a7939d

idea/src/org/jetbrains/kotlin/idea/quickfix/ExperimentalAnnotationTargetFixesFactory.kt
@@ -1,0 +1,100 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.kotlin.idea.quickfix
+
+import com.intellij.codeInsight.intention.HighPriorityAction
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.psi.SmartPsiElementPointer
+import org.jetbrains.kotlin.descriptors.annotations.AnnotationUseSiteTarget
+import org.jetbrains.kotlin.diagnostics.Diagnostic
+import org.jetbrains.kotlin.diagnostics.Errors
+import org.jetbrains.kotlin.idea.KotlinBundle
+import org.jetbrains.kotlin.idea.caches.resolve.analyze
+import org.jetbrains.kotlin.idea.inspections.RemoveAnnotationFix
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi.KtAnnotationEntry
+import org.jetbrains.kotlin.psi.KtModifierListOwner
+import org.jetbrains.kotlin.psi.KtParameter
+import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.psi.psiUtil.createSmartPointer
+import org.jetbrains.kotlin.psi.psiUtil.getParentOfTypes
+import org.jetbrains.kotlin.renderer.render
+import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
+import org.jetbrains.kotlin.utils.addToStdlib.safeAs
+
+/**
+ * Quick fix factory for forbidden experimental annotation usage.
+ *
+ * Annotations that indicate the opt-in requirement are forbidden in several use sites:
+ * getters, value parameters, local variables. This factory generates quick fixes
+ * to either remove the annotation or to replace it with an allowed annotation variant
+ * (e.g., annotate a property instead of a getter or a value parameter).
+ */
+object ExperimentalAnnotationWrongTargetFixesFactory : KotlinIntentionActionsFactory() {
+    override fun doCreateActions(diagnostic: Diagnostic): List<IntentionAction> {
+        if (diagnostic.factory != Errors.OPT_IN_MARKER_ON_WRONG_TARGET) return emptyList()
+        val annotationEntry = diagnostic.psiElement.safeAs<KtAnnotationEntry>() ?: return emptyList()
+        val annotationUseSiteTarget = annotationEntry.useSiteTarget?.getAnnotationUseSiteTarget()
+        val annotatedElement = annotationEntry.getParentOfTypes(
+            strict = true,
+            KtProperty::class.java,
+            KtParameter::class.java,
+        ) ?: return emptyList()
+
+        val result = mutableListOf<IntentionAction>()
+        val bindingContext = annotationEntry.analyze(BodyResolveMode.PARTIAL)
+        val annotationFqName = bindingContext[BindingContext.ANNOTATION, annotationEntry]?.fqName
+        if (annotationFqName != null) {
+            when {
+                annotatedElement is KtParameter && annotationUseSiteTarget != AnnotationUseSiteTarget.PROPERTY ->
+                    result.add(
+                        HighPriorityReplaceAnnotationFix(
+                            annotationEntry,
+                            annotatedElement.createSmartPointer(),
+                            annotationFqName,
+                            useSiteTarget = AnnotationUseSiteTarget.PROPERTY
+                        )
+                    )
+                annotatedElement is KtProperty ->
+                    result.add(
+                        HighPriorityMoveGetterAnnotationToPropertyFix(
+                            annotationEntry,
+                            annotatedElement.createSmartPointer(),
+                            annotationFqName
+                        )
+                    )
+            }
+        }
+
+        // 'Remove annotation' action is the universal fallback
+        result.addAll(RemoveAnnotationFix.createActions(diagnostic))
+        return result
+    }
+}
+
+/**
+ * High priority specialized version of [ReplaceAnnotationFix] with a custom description.
+ * It is used to move annotations from getters to corresponding properties.
+ *
+ * @param annotationEntry the annotation entry to move
+ * @param modifierListOwner the property whose getter is currently annotated
+ * @param annotationFqName fully qualified annotation class name
+ * @param existingReplacementAnnotationEntry the existing annotation to update (null by default)
+ */
+private class HighPriorityMoveGetterAnnotationToPropertyFix(
+    annotationEntry: KtAnnotationEntry,
+    modifierListOwner: SmartPsiElementPointer<KtModifierListOwner>,
+    annotationFqName: FqName,
+    existingReplacementAnnotationEntry: SmartPsiElementPointer<KtAnnotationEntry>? = null
+) : ReplaceAnnotationFix(
+    annotationEntry,
+    modifierListOwner,
+    annotationFqName,
+    argumentClassFqName = null,
+    useSiteTarget = null,
+    existingReplacementAnnotationEntry = existingReplacementAnnotationEntry
+), HighPriorityAction {
+    override fun getText(): String {
+        return KotlinBundle.message("fix.replace.annotation.move.from.getter.to.property", renderAnnotationText())
+    }
+}

idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
@@ -620,6 +620,11 @@ class QuickFixRegistrar : QuickFixContributor {
         OPT_IN_OVERRIDE.registerFactory(ExperimentalFixesFactory)
         OPT_IN_OVERRIDE_ERROR.registerFactory(ExperimentalFixesFactory)
         OPT_IN_IS_NOT_ENABLED.registerFactory(MakeModuleExperimentalFix)
+        OPT_IN_MARKER_ON_WRONG_TARGET.registerFactory(ExperimentalAnnotationWrongTargetFixesFactory)
+        OPT_IN_MARKER_WITH_WRONG_TARGET.registerFactory(RemoveWrongOptInAnnotationTargetFactory)
+        OPT_IN_MARKER_ON_OVERRIDE.registerFactory(RemoveAnnotationFix)
+        OPT_IN_MARKER_ON_OVERRIDE_WARNING.registerFactory(RemoveAnnotationFix)
+
 
         TYPE_VARIANCE_CONFLICT.registerFactory(RemoveTypeVarianceFix)
 

idea/src/org/jetbrains/kotlin/idea/quickfix/RemoveWrongOptInAnnotationTargetFactory.kt
@@ -1,0 +1,49 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.kotlin.idea.quickfix
+
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import org.jetbrains.kotlin.diagnostics.Diagnostic
+import org.jetbrains.kotlin.diagnostics.Errors.OPT_IN_MARKER_WITH_WRONG_TARGET
+import org.jetbrains.kotlin.idea.KotlinBundle
+import org.jetbrains.kotlin.psi.KtAnnotationEntry
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtValueArgument
+import org.jetbrains.kotlin.utils.addToStdlib.safeAs
+
+object RemoveWrongOptInAnnotationTargetFactory : KotlinIntentionActionsFactory() {
+    override fun doCreateActions(diagnostic: Diagnostic): List<IntentionAction> {
+        if (diagnostic.factory != OPT_IN_MARKER_WITH_WRONG_TARGET) return emptyList()
+        val annotationEntry = diagnostic.psiElement.safeAs<KtAnnotationEntry>() ?: return emptyList()
+        return listOf(RemoveAllForbiddenOptInTargetsFix(annotationEntry))
+    }
+
+    private class RemoveAllForbiddenOptInTargetsFix(annotationEntry: KtAnnotationEntry) :
+        KotlinQuickFixAction<KtAnnotationEntry>(annotationEntry) {
+        private val forbiddenTargetValues: List<String> = listOf(
+            AnnotationTarget.EXPRESSION.toString(),
+            AnnotationTarget.FILE.toString(),
+            AnnotationTarget.TYPE.toString(),
+            AnnotationTarget.TYPE_PARAMETER.toString()
+        )
+
+        override fun getText(): String {
+            return KotlinBundle.message("fix.opt_in.remove.all.forbidden.targets")
+        }
+
+        override fun getFamilyName(): String = text
+
+        override fun invoke(project: Project, editor: Editor?, file: KtFile) {
+            val annotationEntry = element ?: return
+            val argumentList = annotationEntry.valueArgumentList ?: return
+            val forbiddenArguments: List<KtValueArgument> = argumentList.arguments.filter {
+                forbiddenTargetValues.any { name -> it.text?.contains(name) == true }
+            }
+
+            forbiddenArguments.forEach {
+                argumentList.removeArgument(it)
+            }
+        }
+    }
+}