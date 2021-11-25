consulo/consulo-kotlin

[tests] fix AbstractJava8BasicCompletionTest
GitOrigin-RevId: 795a8cd4c770cb5b161cf7ab2b1599b7b5a55fa6

completion/tests/test/org/jetbrains/kotlin/idea/completion/test/AbstractJava8BasicCompletionTest.kt
@@ -1,26 +1,16 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.idea.completion.test;
+package org.jetbrains.kotlin.idea.completion.test
 
-import com.intellij.testFramework.LightProjectDescriptor;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor;
-import org.jetbrains.kotlin.platform.TargetPlatform;
-import org.jetbrains.kotlin.platform.jvm.JvmPlatforms;
+import com.intellij.testFramework.LightProjectDescriptor
+import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor
+import org.jetbrains.kotlin.platform.TargetPlatform
+import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
 
-public abstract class AbstractJava8BasicCompletionTest extends AbstractJvmBasicCompletionTest {
-    @NotNull
-    @Override
-    protected LightProjectDescriptor getProjectDescriptor() {
-        return KotlinWithJdkAndRuntimeLightProjectDescriptor.INSTANCE_FULL_JDK;
-    }
-
-    @NotNull
-    @Override
-    public TargetPlatform getPlatform() {
-        return JvmPlatforms.INSTANCE.getJvm18();
-    }
+abstract class AbstractJava8BasicCompletionTest : AbstractJvmBasicCompletionTest() {
+  override fun getProjectDescriptor(): LightProjectDescriptor = KotlinWithJdkAndRuntimeLightProjectDescriptor.INSTANCE
+  override fun getPlatform(): TargetPlatform = JvmPlatforms.jvm18
 }

JetBrains/intellij-kotlin

(cherry picked from commit 795a8cd4c770cb5b161cf7ab2b1599b7b5a55fa6)
GitOrigin-RevId: 717d9f33035fedf721b0fbaf2ff1aa95d5fa8f45

completion/tests/test/org/jetbrains/kotlin/idea/completion/test/AbstractJava8BasicCompletionTest.kt
@@ -1,26 +1,16 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.idea.completion.test;
+package org.jetbrains.kotlin.idea.completion.test
 
-import com.intellij.testFramework.LightProjectDescriptor;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor;
-import org.jetbrains.kotlin.platform.TargetPlatform;
-import org.jetbrains.kotlin.platform.jvm.JvmPlatforms;
+import com.intellij.testFramework.LightProjectDescriptor
+import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor
+import org.jetbrains.kotlin.platform.TargetPlatform
+import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
 
-public abstract class AbstractJava8BasicCompletionTest extends AbstractJvmBasicCompletionTest {
-    @NotNull
-    @Override
-    protected LightProjectDescriptor getProjectDescriptor() {
-        return KotlinWithJdkAndRuntimeLightProjectDescriptor.INSTANCE_FULL_JDK;
-    }
-
-    @NotNull
-    @Override
-    public TargetPlatform getPlatform() {
-        return JvmPlatforms.INSTANCE.getJvm8();
-    }
+abstract class AbstractJava8BasicCompletionTest : AbstractJvmBasicCompletionTest() {
+    override fun getProjectDescriptor(): LightProjectDescriptor = KotlinWithJdkAndRuntimeLightProjectDescriptor.INSTANCE
+    override fun getPlatform(): TargetPlatform = JvmPlatforms.jvm8
 }