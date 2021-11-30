fa10d9a495f0c690325bbd1fc09809ead620b526
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-11-30 12:19:14 +0100

    [kotlin] mpp: "Add modifier" fix should show warning if it could potentially break code
    ^KTIJ-12578 Fixed
    
    (cherry picked from commit 89eb496c009fe95f653f1aa221025fdfe6729caf)
    
    S.Erokhin notes during kt-212-master cherry-pick:
    In kt-212-master the original AddModifierFix was renamed to AddModifierFixMpp
    and after that divided into AddModifierFixMpp and AddModifierFix in
    the ab966d099c58084043a0be0518d83e8f754df6a5 commit.
    
    (cherry picked from commit 991cb0043684b751af06cf1a88aa5e27e34614c1)
    
    GitOrigin-RevId: 7eb15e691ae22507545dd4dbc392e544a138a315

--- idea/tests/test/org/jetbrains/kotlin/idea/quickfix/QuickFixMultiModuleTestGenerated.java
+++ idea/tests/test/org/jetbrains/kotlin/idea/quickfix/QuickFixMultiModuleTestGenerated.java
@@ -1008,6 +1008,11 @@ public abstract class QuickFixMultiModuleTestGenerated extends AbstractQuickFixM
             runTest("testData/multiModuleQuickFix/other/addOperatorByExpect/");
         }
 
+        @TestMetadata("cancelMakeAbstractFromActual")
+        public void testCancelMakeAbstractFromActual() throws Exception {
+            runTest("testData/multiModuleQuickFix/other/cancelMakeAbstractFromActual/");
+        }
+
         @TestMetadata("convertActualEnumToSealedClass")
         public void testConvertActualEnumToSealedClass() throws Exception {
             runTest("testData/multiModuleQuickFix/other/convertActualEnumToSealedClass/");
@@ -1173,6 +1178,11 @@ public abstract class QuickFixMultiModuleTestGenerated extends AbstractQuickFixM
             runTest("testData/multiModuleQuickFix/other/importFunInCommon/");
         }
 
+        @TestMetadata("makeAbstractFromActual")
+        public void testMakeAbstractFromActual() throws Exception {
+            runTest("testData/multiModuleQuickFix/other/makeAbstractFromActual/");
+        }
+
         @TestMetadata("makeInlineFromExpect")
         public void testMakeInlineFromExpect() throws Exception {
             runTest("testData/multiModuleQuickFix/other/makeInlineFromExpect/");
@@ -1251,6 +1261,11 @@ public abstract class QuickFixMultiModuleTestGenerated extends AbstractQuickFixM
         @TestMetadata("movePropertyToConstructor")
         public void testMovePropertyToConstructor() throws Exception {
             runTest("testData/multiModuleQuickFix/other/movePropertyToConstructor/");
+        }
+
+        @TestMetadata("notMakeAbstractFromActual")
+        public void testNotMakeAbstractFromActual() throws Exception {
+            runTest("testData/multiModuleQuickFix/other/notMakeAbstractFromActual/");
         }
 
         @TestMetadata("orderHeader")
