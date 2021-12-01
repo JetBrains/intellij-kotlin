be7d6cad9ee1586f0f5c2af939ce8e558532aafc
Dmitry Timofeev <dmitry.timofeev@jetbrains.com>
2021-12-01 15:54:53 +0100

    [kotlin] Add opt-in markers to extracted functions when necessary
    If the extracted code references an experimental API, and the function
    container is annotated with corresponding propagating opt-in markers or
    the `@OptIn` annotation, these annotations should be added to the
    generated function declaration as well. Without these annotations,
    the updated code may stop compiling or would start to cause compiler
    warnings.
    
    This commit adds opt-in annotation tracking to 'Extract Function'
    action: the minimal necessary subset of original annotations is
    added to the extracted function. Propagating markers are generated if
    original markers were propagating, and `@OptIn` annotation is generated
    for non-propagating opted-in markers.
    
    ^KTIJ-15823 Fixed
    
    (cherry picked from commit 5db0b69092c21d5f0e4fb7f2f23e3959ef812970)
    
    GitOrigin-RevId: 029752b3d64a9f17e3da6c094c41f63686d80354

--- idea/src/org/jetbrains/kotlin/idea/refactoring/introduce/extractionEngine/ExtractableCodeDescriptor.kt
+++ idea/src/org/jetbrains/kotlin/idea/refactoring/introduce/extractionEngine/ExtractableCodeDescriptor.kt
@@ -359,7 +359,8 @@ data class ExtractableCodeDescriptor(
     val controlFlow: ControlFlow,
     val returnType: KotlinType,
     val modifiers: List<KtKeywordToken> = emptyList(),
-    val annotations: List<AnnotationDescriptor> = emptyList()
+    val annotations: List<AnnotationDescriptor> = emptyList(),
+    val optInMarkers: List<FqName> = emptyList()
 ) {
     val name: String get() = suggestedNames.firstOrNull() ?: ""
     val duplicates: List<DuplicateInfo> by lazy { findDuplicates() }
@@ -396,7 +397,8 @@ fun ExtractableCodeDescriptor.copy(
         controlFlow.copy(oldToNewParameters),
         returnType ?: this.returnType,
         modifiers,
-        annotations
+        annotations,
+        optInMarkers
     )
 }
 
