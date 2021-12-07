JetBrains/intellij-community

[kotlin] Support older compilers in MakeModuleExperimentalFix
^KTIJ-20171 Fixed

IJ-CR-16768

GitOrigin-RevId: 67061acc42858694b2ed282df52d4a12d7f7218c

plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/MakeModuleExperimentalFix.kt
@@ -7,40 +7,50 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProviderImpl
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
-import org.jetbrains.kotlin.config.ApiVersion
 import org.jetbrains.kotlin.config.CompilerSettings
 import org.jetbrains.kotlin.diagnostics.Diagnostic
 import org.jetbrains.kotlin.idea.KotlinBundle
 import org.jetbrains.kotlin.idea.caches.project.toDescriptor
 import org.jetbrains.kotlin.idea.configuration.BuildSystemType
 import org.jetbrains.kotlin.idea.configuration.getBuildSystemType
-import org.jetbrains.kotlin.idea.facet.KotlinFacet
 import org.jetbrains.kotlin.idea.facet.getOrCreateConfiguredFacet
-import org.jetbrains.kotlin.idea.project.languageVersionSettings
 import org.jetbrains.kotlin.idea.quickfix.ExperimentalFixesFactory.fqNameIsExisting
 import org.jetbrains.kotlin.idea.roots.invalidateProjectRoots
 import org.jetbrains.kotlin.idea.util.projectStructure.module
+import org.jetbrains.kotlin.idea.versions.kotlinCompilerVersionShort
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.resolve.checkers.OptInNames
+import org.jetbrains.kotlin.utils.KotlinExceptionWithAttachments
+import org.jetbrains.kotlin.utils.checkWithAttachment
 
+/**
+ * A quick fix that adds an opt-in compiler argument to the current module configuration facet (JPS only).
+ */
 open class MakeModuleExperimentalFix(
     file: KtFile,
     private val module: Module,
     annotationFqName: FqName
 ) : KotlinQuickFixAction<KtFile>(file) {
 
-    private val experimentalPrefix =
-        if (module.toDescriptor()?.fqNameIsExisting(OptInNames.REQUIRES_OPT_IN_FQ_NAME) == true)
-            "-opt-in"
-        else
-            "-Xuse-experimental"
+    // The actual name of the opt-in compiler argument depends on the Kotlin compiler version
+    // `-opt-in` (since Kotlin 1.6) https://youtrack.jetbrains.com/issue/KT-47099
+    // `-Xopt-in` (before Kotlin 1.6) https://blog.jetbrains.com/kotlin/2020/03/kotlin-1-3-70-released/
+    // `-Xuse-experimental` (before Kotlin 1.3.70), a fallback if `RequireOptIn` annotation does not exist
+
+    private val kotlinCompilerVersion = KotlinVersion.fromString(kotlinCompilerVersionShort())
+
+    private val experimentalPrefix = when {
+        module.toDescriptor()?.fqNameIsExisting(OptInNames.REQUIRES_OPT_IN_FQ_NAME) == false -> "-Xuse-experimental"
+        kotlinCompilerVersion.isAtLeast(1, 6, 0) -> "-opt-in"
+        else -> "-Xopt-in"
+    }
+
+    private val compilerArgument = "$experimentalPrefix=$annotationFqName"
 
     override fun getText(): String = KotlinBundle.message("add.0.to.module.1.compiler.arguments", compilerArgument, module.name)
 
     override fun getFamilyName(): String = KotlinBundle.message("add.an.opt.in.requirement.marker.compiler.argument")
-
-    private val compilerArgument = "$experimentalPrefix=$annotationFqName"
 
     override fun invoke(project: Project, editor: Editor?, file: KtFile) {
         val modelsProvider = IdeModifiableModelsProviderImpl(project)
@@ -79,4 +89,10 @@ open class MakeModuleExperimentalFix(
             )
         }
     }
+}
+
+private fun KotlinVersion.Companion.fromString(version: String): KotlinVersion {
+    val parts = version.split(".")
+    require(parts.size == 3)
+    return KotlinVersion(parts[0].toInt(), parts[1].toInt(), parts[2].toInt())
 }

JetBrains/intellij-kotlin

JPS compiler version is used if possible, otherwise the Kotlin stdlib
version.
`kotlinCompilerVersionShort()` updated to support '-dev' version suffix.
(cherry picked from commit 05eeef21c508d894ad9a333cebe263f8161b22b4)
GitOrigin-RevId: f0e2ef7f57b16d066e1cf30e3fbbc827ffd76290

idea/src/org/jetbrains/kotlin/idea/quickfix/MakeModuleExperimentalFix.kt
@@ -10,40 +10,49 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProviderImpl
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
-import org.jetbrains.kotlin.config.ApiVersion
 import org.jetbrains.kotlin.config.CompilerSettings
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
 import org.jetbrains.kotlin.diagnostics.Diagnostic
 import org.jetbrains.kotlin.idea.KotlinBundle
 import org.jetbrains.kotlin.idea.caches.project.toDescriptor
 import org.jetbrains.kotlin.idea.configuration.BuildSystemType
 import org.jetbrains.kotlin.idea.configuration.getBuildSystemType
-import org.jetbrains.kotlin.idea.facet.KotlinFacet
 import org.jetbrains.kotlin.idea.facet.getOrCreateConfiguredFacet
-import org.jetbrains.kotlin.idea.project.languageVersionSettings
 import org.jetbrains.kotlin.idea.quickfix.ExperimentalFixesFactory.fqNameIsExisting
 import org.jetbrains.kotlin.idea.roots.invalidateProjectRoots
 import org.jetbrains.kotlin.idea.util.projectStructure.module
+import org.jetbrains.kotlin.idea.versions.fromString
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.resolve.checkers.OptInNames
 
+/**
+ * A quick fix that adds an opt-in compiler argument to the current module configuration facet (JPS only).
+ */
 open class MakeModuleExperimentalFix(
     file: KtFile,
     private val module: Module,
     annotationFqName: FqName
 ) : KotlinQuickFixAction<KtFile>(file) {
 
-    private val experimentalPrefix =
-        if (module.toDescriptor()?.fqNameIsExisting(OptInNames.REQUIRES_OPT_IN_FQ_NAME) == true)
-            "-opt-in"
-        else
-            "-Xuse-experimental"
+    // The actual name of the opt-in compiler argument depends on the Kotlin compiler version
+    // `-opt-in` (since Kotlin 1.6) https://youtrack.jetbrains.com/issue/KT-47099
+    // `-Xopt-in` (before Kotlin 1.6) https://blog.jetbrains.com/kotlin/2020/03/kotlin-1-3-70-released/
+    // `-Xuse-experimental` (before Kotlin 1.3.70), a fallback if `RequireOptIn` annotation does not exist
+
+    private val kotlinCompilerVersion = KotlinVersion.fromString(KotlinCompilerVersion.VERSION) ?: KotlinVersion.CURRENT
+
+    private val experimentalPrefix = when {
+        module.toDescriptor()?.fqNameIsExisting(OptInNames.REQUIRES_OPT_IN_FQ_NAME) == false -> "-Xuse-experimental"
+        kotlinCompilerVersion.isAtLeast(1, 6, 0) -> "-opt-in"
+        else -> "-Xopt-in"
+    }
+
+    private val compilerArgument = "$experimentalPrefix=$annotationFqName"
 
     override fun getText(): String = KotlinBundle.message("add.0.to.module.1.compiler.arguments", compilerArgument, module.name)
 
     override fun getFamilyName(): String = KotlinBundle.message("add.an.opt.in.requirement.marker.compiler.argument")
-
-    private val compilerArgument = "$experimentalPrefix=$annotationFqName"
 
     override fun invoke(project: Project, editor: Editor?, file: KtFile) {
         val modelsProvider = IdeModifiableModelsProviderImpl(project)