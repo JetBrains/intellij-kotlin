JetBrains/intellij-community

[Kotlin][MPP] Implement MultiplatformAnalysisTestGenerated.testDefaultArguments
^KT-50120

GitOrigin-RevId: 4884787971c2945ed1fc7c9a46cba7059a9043e4

plugins/kotlin/idea/tests/testData/multiplatform/defaultArguments/middle/middle.kt
@@ -1,0 +1,35 @@
+package sample
+
+import sample.A.Companion.companionExtensionFunction
+
+actual class <!LINE_MARKER("descr='Has declaration in common module'")!>A<!> {
+    actual fun <!LINE_MARKER("descr='Has declaration in common module'")!>memberFunction<!>(x: Int, y: String) = Unit
+    actual companion <!LINE_MARKER("descr='Has declaration in common module'")!>object<!> {
+        actual fun <!LINE_MARKER("descr='Has declaration in common module'")!>companionFunction<!>(x: Int, y: String) = Unit
+        actual fun String.<!LINE_MARKER("descr='Has declaration in common module'")!>companionExtensionFunction<!>(x: Int, y: String) = Unit
+    }
+}
+
+actual fun <!LINE_MARKER("descr='Has declaration in common module'")!>topLevelFunction<!>(x: Int, y: String) = Unit
+
+actual fun String.<!LINE_MARKER("descr='Has declaration in common module'")!>topLevelExtensionFunction<!>(x: Int, y: String) = Unit
+
+fun testMiddle() {
+    A().memberFunction(<!NO_VALUE_FOR_PARAMETER!>)<!>
+    A().memberFunction(42)
+    A().memberFunction(42, "ok")
+
+    topLevelFunction(<!NO_VALUE_FOR_PARAMETER!>)<!>
+    topLevelFunction(42)
+    topLevelFunction(42, "ok")
+
+    "".topLevelExtensionFunction(<!NO_VALUE_FOR_PARAMETER!>)<!>
+    "".topLevelExtensionFunction(42)
+    "".topLevelExtensionFunction(42, "ok")
+}
+
+fun A.testMiddle() {
+    "".companionExtensionFunction(<!NO_VALUE_FOR_PARAMETER!>)<!>
+    "".companionExtensionFunction(42)
+    "".companionExtensionFunction(42, "ok")
+}

JetBrains/intellij-kotlin

(cherry picked from commit cdc964cdab50a813b168b5982717c4419a53c5cc)
GitOrigin-RevId: d59682b3f1b5204da054adb1e868c300a6a5573f

idea/tests/testData/multiplatform/defaultArguments/middle/middle.kt
@@ -1,0 +1,35 @@
+package sample
+
+import sample.A.Companion.companionExtensionFunction
+
+actual class <!LINE_MARKER("descr='Has expects in top module'")!>A<!> {
+    actual fun <!LINE_MARKER("descr='Has expects in top module'")!>memberFunction<!>(x: Int, y: String) = Unit
+    actual companion <!LINE_MARKER("descr='Has expects in top module'")!>object<!> {
+        actual fun <!LINE_MARKER("descr='Has expects in top module'")!>companionFunction<!>(x: Int, y: String) = Unit
+        actual fun String.<!LINE_MARKER("descr='Has expects in top module'")!>companionExtensionFunction<!>(x: Int, y: String) = Unit
+    }
+}
+
+actual fun <!LINE_MARKER("descr='Has expects in top module'")!>topLevelFunction<!>(x: Int, y: String) = Unit
+
+actual fun String.<!LINE_MARKER("descr='Has expects in top module'")!>topLevelExtensionFunction<!>(x: Int, y: String) = Unit
+
+fun testMiddle() {
+    A().memberFunction(<!NO_VALUE_FOR_PARAMETER!>)<!>
+    A().memberFunction(42)
+    A().memberFunction(42, "ok")
+
+    topLevelFunction(<!NO_VALUE_FOR_PARAMETER!>)<!>
+    topLevelFunction(42)
+    topLevelFunction(42, "ok")
+
+    "".topLevelExtensionFunction(<!NO_VALUE_FOR_PARAMETER!>)<!>
+    "".topLevelExtensionFunction(42)
+    "".topLevelExtensionFunction(42, "ok")
+}
+
+fun A.testMiddle() {
+    "".companionExtensionFunction(<!NO_VALUE_FOR_PARAMETER!>)<!>
+    "".companionExtensionFunction(42)
+    "".companionExtensionFunction(42, "ok")
+}