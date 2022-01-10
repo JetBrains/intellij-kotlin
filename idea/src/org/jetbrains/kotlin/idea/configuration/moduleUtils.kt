88ccf953f9828f6174c613adc55d937fcd8676a3
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-01-10 06:57:10 +0100

    [kotlin] replace redundant KotlinExternalCompilerVersionService with util extension
    ^KTIJ-20169
    
    (cherry picked from commit 774952cb7b184d1764b6e2cfd198abcb08ea2f62)
    (cherry picked from commit cb16f22c664deda7a830e985db23179979edc785)
    
    GitOrigin-RevId: 9b430b0cd42922cc699024c76f430c06bd5da486

--- idea/src/org/jetbrains/kotlin/idea/configuration/moduleUtils.kt
+++ idea/src/org/jetbrains/kotlin/idea/configuration/moduleUtils.kt
@@ -7,7 +7,19 @@ package org.jetbrains.kotlin.idea.configuration
 
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.util.Key
+import org.jetbrains.kotlin.idea.KotlinVersionVerbose
+import org.jetbrains.kotlin.idea.util.buildNumber
 import org.jetbrains.kotlin.psi.UserDataProperty
 
 var Module.externalCompilerVersion: String? by UserDataProperty(Key.create("EXTERNAL_COMPILER_VERSION"))
+
+fun Module.findExternalKotlinCompilerVersion(): KotlinVersionVerbose? {
+    val externalCompilerVersion = (if (getBuildSystemType() == BuildSystemType.JPS) {
+        buildNumber
+    } else {
+        this.externalCompilerVersion
+    }) ?: return null
+
+    return KotlinVersionVerbose.parse(externalCompilerVersion)
+}
 
--- idea/src/org/jetbrains/kotlin/idea/configuration/projectUtils.kt
+++ idea/src/org/jetbrains/kotlin/idea/configuration/projectUtils.kt
@@ -6,8 +6,12 @@
 package org.jetbrains.kotlin.idea.configuration
 
 import com.intellij.openapi.module.Module
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.project.Project
 import com.intellij.psi.search.FileTypeIndex
 import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.idea.KotlinVersionVerbose
+import org.jetbrains.kotlin.util.firstNotNullResult
 
 fun hasKotlinFilesOnlyInTests(module: Module): Boolean {
     return !hasKotlinFilesInSources(module) && FileTypeIndex.containsFileOfType(KotlinFileType.INSTANCE, module.getModuleScope(true))
@@ -16,3 +20,6 @@ fun hasKotlinFilesOnlyInTests(module: Module): Boolean {
 fun hasKotlinFilesInSources(module: Module): Boolean {
     return FileTypeIndex.containsFileOfType(KotlinFileType.INSTANCE, module.getModuleScope(false))
 }
+
+fun Project.findAnyExternalKotlinCompilerVersion(): KotlinVersionVerbose? =
+    ModuleManager.getInstance(this).modules.firstNotNullResult { it.findExternalKotlinCompilerVersion() }
