2c4751476799dcbc38d7a96e1874c233cd90aab4
Nikita Bobko <nikita.bobko@jetbrains.com>
2022-01-18 02:34:11 +0100

    2/2 Remove Kotlin JPS plugin from intellij repo
    Kotlin JPS plugin is going to be merged to Kotlin repo
    in scope of KTIJ-11633. This commit drops Kotlin JPS
    plugin sources from intellij repo
    
    (cherry picked from commit 0b3f719718caad119040d6b2b51a1e4ea0ba640e)
    
    Re-created by S.Erokhin. Some links to kotlin.jps-common is still in KotlinPluginCommunity.xml
    
    GitOrigin-RevId: 758fba7912d5749732ea44eae9e02dd1ac2b2fe2

--- jps/jps-plugin/testData/incremental/withJava/javaUsedInKotlin/constantPropertyChanged/JavaClass.java
+++
@@ -1,8 +0,0 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-public class JavaClass {
-    public class Inner {
-        public class InnerInner {
-            public static final String CONST = "A";
-        }
-    }
-}
