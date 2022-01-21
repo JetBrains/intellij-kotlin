898fe127ebb686e01e6dad6cb8726837c0bdd8c7
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2022-01-21 01:45:42 +0100

    [kt-211-1.6.20* only] Testdata: ProjectTemplateBuildFileGenerationTestGenerated
    Unfortunately in 211 branch "keepKotlinVersion true" is not supported, so
    because right now KOTLIN_VERSION_FOR_COMPOSE is 1.6.10 and the last released kotlin is 1.6.10
    these tests demands usage of template KOTLIN_VERSION.
    Once 1.6.20-M1 is released, test will fail, and we could partially revert this commit
    
    GitOrigin-RevId: 1e887d2f42cb273178b8d2a10e181fe3c4ba85ea

--- project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeDesktopApplication/build.gradle.kts
+++ project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeDesktopApplication/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "KOTLIN_VERSION"
     id("org.jetbrains.compose") version "1.0.1"
 }
 
--- project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeMultiplatformApplication/expected/build.gradle.kts
+++ project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeMultiplatformApplication/expected/build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:KOTLIN_VERSION")
         classpath("com.android.tools.build:gradle:4.1.2")
     }
 }
